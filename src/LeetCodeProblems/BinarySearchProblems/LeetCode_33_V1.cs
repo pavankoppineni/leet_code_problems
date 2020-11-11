71baca98416723ae7a0a9d28bc774ce22d65f894
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-11-12 00:41:58 +0530

    Added more test cases

---
+++ src/LeetCodeProblems/BinarySearchProblems/LeetCode_33_V1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BinarySearchProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/search-in-rotated-sorted-array/
+    /// </summary>
+    public class LeetCode_33_V1
+    {
+        private int[] values;
+        private int target;
+
+        public int SearchElement(int[] values, int target)
+        {
+            this.values = values;
+            this.target = target;
+
+            var pivotIndex = FindPivot(0, values.Length - 1);
+
+            if (pivotIndex == 0)
+            {
+                return SearchElement(0, values.Length - 1);
+            }
+
+            if (target >= values[0])
+            {
+                return SearchElement(0, pivotIndex - 1);
+            }
+
+            return SearchElement(pivotIndex, values.Length - 1);
+        }
+
+        private int SearchElement(int low, int high)
+        {
+            var mid = (low + high) >> 1;
+
+            //Case 1 : Mid is equal to target
+            //Action : Return mid index
+            if (values[mid] == target)
+            {
+                return mid;
+            }
+
+            //Case 2 : Mid is greater than target
+            //Action : Search left
+            else if (values[mid] > target)
+            {
+                return SearchElement(low, mid - 1);
+            }
+            //Case 3 : Mid is less than target
+            //Action : Search right
+            else
+            {
+                return SearchElement(mid + 1, high);
+            }
+        }
+
+        /// <summary>
+        /// Method to find pivot element in sorted rotated array
+        /// </summary>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns>Return</returns>
+        private int FindPivot(int low, int high)
+        {
+            if (low == high)
+            {
+                return low;
+            }
+
+            var mid = (low + high) >> 1;
+            //Case 1 : Mid is less than high
+            //Action : Search left
+            if (values[mid] < values[high])
+            {
+                return FindPivot(low, mid);
+            }
+            //Case 2 : Mid is greater than high
+            //Action : Search right
+            else
+            {
+                return FindPivot(mid + 1, high);
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BinarySearchProblems/LeetCode_33_V1Tests.cs
@@ -0,0 +1,101 @@
+﻿using LeetCodeProblems.BinarySearchProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BinarySearchProblems
+{
+    [TestClass]
+    public class LeetCode_33_V1Tests
+    {
+        private static IList<object[]> GetTestData()
+        {
+            var dataset = new List<object[]>();
+            //Test Case One : Array is not rotated
+            dataset.Add(new object[]
+            {
+                //Array
+                new int[] { 1, 2, 3, 4, 5},
+
+                //Target element
+                3,
+
+                //Expected index
+                2
+            });
+
+            //Test Case Two : Array is rotated, pivot is to the right side of array
+            // and target element is to right side of array
+            dataset.Add(new object[]
+            {
+                //Rotated sorted array
+                new int[] { 3, 4, 5, 1, 2},
+
+                //Target element
+                2,
+
+                //Expected index
+                4
+            });
+
+            //Test Case Three : Array is rotated, pivot is to the right side of array
+            // and target element is to left side of array
+            dataset.Add(new object[]
+            {
+                //Rotated sorted array
+                new int[] { 3, 4, 5, 1, 2},
+
+                //Target element
+                4,
+
+                //Expected index
+                1
+            });
+
+            //Test Case Four : Array is rotated, pivot is to the left side of array
+            // and target element is to left side of array
+            dataset.Add(new object[]
+            {
+                //Rotated sorted array
+                new int[] { 5, 1, 2, 3, 4},
+
+                //Target element
+                1,
+
+                //Expected index
+                1
+            });
+
+            //Test Case Four : Array is rotated, pivot is to the left side of array
+            // and target element is to right side of array
+            dataset.Add(new object[]
+            {
+                //Rotated sorted array
+                new int[] { 5, 1, 2, 3, 4},
+
+                //Target element
+                3,
+
+                //Expected index
+                3
+            });
+
+            return dataset;
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void Given_RotatedSortedArrayAndTargetNumber_When_SearchElement_Then_ShouldReturnIndexOfTargetElement(int[] rotatedArray, int target, int expectedIndex)
+        {
+            //Given
+            var problem = new LeetCode_33_V1();
+
+            //When
+            var actualIndex = problem.SearchElement(rotatedArray, target);
+
+            //Then
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+    }
+}
