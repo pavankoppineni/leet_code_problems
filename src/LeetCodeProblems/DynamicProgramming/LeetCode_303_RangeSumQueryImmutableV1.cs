4ee290d0b1e23ba3e5cde05c457ec32ccd67ab73
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-11-12 18:09:11 +0530

    Added  range sum problem

---
+++ src/LeetCodeProblems/DynamicProgramming/LeetCode_303_RangeSumQueryImmutableV1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-query-immutable/
+    /// </summary>
+    public class LeetCode_303_RangeSumQueryImmutableV1
+    {
+        private readonly int[] nums;
+        private readonly int[] prefixSum;
+
+        public LeetCode_303_RangeSumQueryImmutableV1(int[] nums)
+        {
+            this.nums = nums;
+            prefixSum = new int[nums.Length];
+            prefixSum[0] = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + nums[i];
+            }
+        }
+
+        public int GetRangeSum(int left, int right)
+        {
+            if (left == 0)
+            {
+                return prefixSum[right];
+            }
+
+            return prefixSum[right] - prefixSum[left - 1];
+        }
+    }
+}
