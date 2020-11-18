37dfad9082181ea90e0c43ad6e682fe0c5fcf544
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-11-18 21:47:50 +0530

    Added new problems (#4)
    * Added  range sum problem
    
    * Update README.md
    
    * Update README.md
    
    * Added new problems
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/LeetCodeProblems/ArrayProblems/LeetCode_307_RangeSumQueryMutable_V1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-query-mutable/
+    /// </summary>
+    public class LeetCode_307_RangeSumQueryMutable_V1
+    {
+        private BinaryIndexedTree binaryIndexedTree;
+
+        public LeetCode_307_RangeSumQueryMutable_V1(int[] nums)
+        {
+            this.binaryIndexedTree = new BinaryIndexedTree(nums);
+            this.binaryIndexedTree.Build();
+        }
+
+        public void Update(int index, int value)
+        {
+            this.binaryIndexedTree.Update(index, value);
+        }
+
+        public int SumRange(int i, int j)
+        {
+            if (i == j)
+            {
+                return binaryIndexedTree[i];
+            }
+
+            if (i == 0)
+            {
+                return binaryIndexedTree.GetSum(j);
+            }
+
+            var sumJ = binaryIndexedTree.GetSum(j);
+            var sumI = binaryIndexedTree.GetSum(i - 1);
+
+            return sumJ - sumI;
+        }
+
+        public class BinaryIndexedTree
+        {
+            private int[] values;
+            private int[] biaryIndexedArray;
+            private int length;
+
+            public BinaryIndexedTree(int[] values)
+            {
+                this.length = values.Length;
+                this.values = values;
+                this.biaryIndexedArray = new int[this.length + 1];
+            }
+
+            public int this[int index]
+            {
+                get
+                {
+                    return values[index];
+                }
+            }
+
+            public void Build()
+            {
+                for (var i = 0; i < length; i++)
+                {
+                    Update(i, values[i], 0);
+                }
+            }
+
+            public void Update(int index, int value)
+            {
+                var previousValue = values[index];
+                values[index] = value;
+                Update(index, value, previousValue);
+            }
+
+            private void Update(int index, int value, int previousValue)
+            {
+                var currentIndex = index + 1;
+                while (currentIndex <= length)
+                {
+                    biaryIndexedArray[currentIndex] += value;
+                    biaryIndexedArray[currentIndex] -= previousValue;
+                    currentIndex += (currentIndex) & (-currentIndex);
+                }
+            }
+
+            public int GetSum(int index)
+            {
+                var sum = 0;
+                var currentIndex = index + 1;
+                while (currentIndex > 0)
+                {
+                    sum += biaryIndexedArray[currentIndex];
+                    currentIndex -= (currentIndex) & (-currentIndex);
+                }
+                return sum;
+            }
+        }
+    }
+}
---
+++ src/LeetCodeProblems/DynamicProgramming/LeetCode_304_RangeSumQuery2DImmutableV1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-query-2d-immutable/
+    /// </summary>
+    public class LeetCode_304_RangeSumQuery2DImmutableV1
+    {
+        private readonly int[][] matrix;
+        private readonly int[][] prefixSumMatrix;
+        private readonly int length;
+        private readonly int rowLength;
+        private readonly int columnLength;
+
+        public LeetCode_304_RangeSumQuery2DImmutableV1(int[][] matrix)
+        {
+            length = matrix.Length;
+            if (length > 0)
+            {
+                this.matrix = matrix;
+                rowLength = this.matrix.GetLength(0);
+                columnLength = this.matrix[0].Length;
+                prefixSumMatrix = new int[rowLength][];
+                CalculatePrefixSum();
+            }
+        }
+
+        public int GetRangeSum(int startRow, int endRow, int startColumn, int endColumn)
+        {
+            if (startRow < 0 || startColumn < 0)
+            {
+                return 0;
+            }
+
+            if (endRow >= rowLength || endColumn >= columnLength)
+            {
+                return 0;
+            }
+
+            return GetSumFromLookUp(endRow, endColumn)
+                - GetSumFromLookUp(startRow - 1, endColumn)
+                - (GetSumFromLookUp(endRow, startColumn - 1) - GetSumFromLookUp(startRow - 1, startColumn - 1));
+        }
+
+        private void CalculatePrefixSum()
+        {
+            for (var row = 0; row < rowLength; row++)
+            {
+                prefixSumMatrix[row] = new int[columnLength];
+                var row_sum = 0;
+                for (var col = 0; col < columnLength; col++)
+                {
+                    row_sum += matrix[row][col];
+                    prefixSumMatrix[row][col] = row_sum + GetSumFromLookUp(row - 1, col);
+                }
+            }
+        }
+
+        private int GetSumFromLookUp(int row, int col)
+        {
+            if (row < 0 || col < 0)
+            {
+                return 0;
+            }
+
+            if (row >= rowLength || col >= columnLength)
+            {
+                return 0;
+            }
+
+            return prefixSumMatrix[row][col];
+        }        
+    }
+}
