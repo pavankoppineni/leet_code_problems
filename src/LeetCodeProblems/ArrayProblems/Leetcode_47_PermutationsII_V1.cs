d4d192781f728389c842ce683919d3ba388455c0
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-12-03 23:16:42 +0530

    Added new problems (#10)
    * Added  range sum problem
    
    * Update README.md
    
    * Update README.md
    
    * Added new problems
    
    * Added new problems
    
    * added new problems
    
    * added new problem
    
    * Added merge intervals problem
    
    * Added new problems
    
    * Added new problems
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_47_PermutationsII_V1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/permutations-ii/
+    /// </summary>
+    public class Leetcode_47_PermutationsII_V1
+    {
+        private int length;
+        private int[] keysArray;
+        private int[] valuesArray;
+        private IList<IList<int>> permutations;
+        private int[] permutationItem;
+
+        public IList<IList<int>> Permutate(int[] nums)
+        {
+            permutations = new List<IList<int>>();
+            length = nums.Length;
+            var numDictionary = new Dictionary<int, int>();
+            permutationItem = new int[nums.Length];
+            foreach (var num in nums)
+            {
+                if (numDictionary.ContainsKey(num))
+                {
+                    numDictionary[num] += 1;
+                }
+                else
+                {
+                    numDictionary.Add(num, 1);
+                }
+            }
+            keysArray = numDictionary.Keys.ToArray();
+            valuesArray = numDictionary.Values.ToArray();
+            Permutate(0);
+            return permutations;
+        }
+
+        private void Permutate(int rank)
+        {
+            if (rank == length)
+            {
+                var currentPermutationItem = new int[length];
+                Array.Copy(permutationItem, 0, currentPermutationItem, 0, length);
+                permutations.Add(currentPermutationItem);
+                return;
+            }
+
+            for (var i = 0; i < keysArray.Length; i++)
+            {
+                if (valuesArray[i] == 0)
+                {
+                    continue;
+                }
+                valuesArray[i] -= 1;
+                permutationItem[rank] = keysArray[i];
+                Permutate(rank + 1);
+                valuesArray[i] += 1;
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_46_Permutations_V1Tests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_46_Permutations_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Permutate_Then_ShouldReturnPermutations()
+        {
+            //Given
+            var nums = new int[] { 1, 2, 3 };
+            var problem = new Leetcode_46_Permutations_V1();
+            var expectedResult = new List<List<int>>
+            {
+                new List<int>{ 1, 2, 3},
+                new List<int>{1, 3, 2},
+                new List<int>{3, 1, 2},
+                new List<int>{3, 2, 1},
+                new List<int>{2, 1, 3},
+                new List<int>{2, 3, 1}
+            };
+
+            //When
+            var actualResult = problem.Permutate(nums);
+
+            //Then
+            Assert.AreEqual(expectedResult.Count, actualResult.Count);
+        }
+    }
+}
