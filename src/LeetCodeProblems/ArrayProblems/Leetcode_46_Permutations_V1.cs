eb52dda7380760015f96b9da60c2d36a97e0c00d
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-12-03 23:14:53 +0530

    Added new problems

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_46_Permutations_V1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/permutations/
+    /// </summary>
+    public class Leetcode_46_Permutations_V1
+    {
+        private IList<IList<int>> permutations;
+        private int[] nums;
+
+        public IList<IList<int>> Permutate(int[] nums)
+        {
+            this.nums = nums;
+            permutations = new List<IList<int>>();
+            Permutate(0);
+            return permutations;
+        }
+
+        private void Permutate(int index)
+        {
+            if (nums.Length == index)
+            {
+                var permutationItem = new List<int>();
+                foreach (var num in nums)
+                {
+                    permutationItem.Add(num);
+                }
+                permutations.Add(permutationItem);
+                return;
+            }
+
+            for (var i = index; i < nums.Length; i++)
+            {
+                Swap(i, index);
+                Permutate(index + 1);
+                Swap(i, index);
+            }
+        }
+
+        private void Swap(int indexA, int indexB)
+        {
+            var temp = nums[indexA];
+            nums[indexA] = nums[indexB];
+            nums[indexB] = temp;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_47_PermutationsII_V1Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_47_PermutationsII_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Permutate_Then_ShouldGeneratePermutations()
+        {
+            //Given
+            var nums = new int[] { 1, 1, 2 };
+            var problem = new Leetcode_47_PermutationsII_V1();
+            var expectedResult = new List<List<int>>
+            {
+                new List<int>{1, 1, 2},
+                new List<int>{1, 2, 1},
+                new List<int>{2, 1, 1},
+            };
+
+            //When
+            var actualResult = problem.Permutate(nums);
+
+            //Then
+            Assert.AreEqual(expectedResult.Count, actualResult.Count);
+        }
+    }
+}
