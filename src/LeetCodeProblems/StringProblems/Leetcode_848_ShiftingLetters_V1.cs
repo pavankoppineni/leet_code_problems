43168e713d5a9de4a132fb027e95edcea2891bd5
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2020-12-10 14:19:37 +0530

    Added new problems

---
+++ src/LeetCodeProblems/StringProblems/Leetcode_848_ShiftingLetters_V1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeProblems.StringProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/shifting-letters/
+    /// </summary>
+    public class Leetcode_848_ShiftingLetters_V1
+    {
+        public string ShiftLetters(string str, int[] shifts)
+        {
+            if (shifts.Length == 0)
+            {
+                return str;
+            }
+
+            var newStr = new char[shifts.Length];
+            var offsetShift = 0;
+            for(var i = shifts.Length - 1; i >=0; i--)
+            {
+                var shiftPosition = shifts[i] % 26;
+                var newChar = ShiftCharacter(str[i], shiftPosition + offsetShift);
+                newStr[i] = newChar;
+                offsetShift += shiftPosition;
+            }
+
+            return new string(newStr);
+        }
+
+        private char ShiftCharacter(char charItem, int shiftPosition)
+        {
+            //Base case
+            if (shiftPosition == 0)
+            {
+                return charItem;
+            }
+
+            var offset = charItem - 'a';
+            shiftPosition %= 26;
+            // Case 1: Shiftposition is zero
+            // Action : Return same chanracter
+            if (shiftPosition == 0)
+            {
+                return charItem;
+            }
+
+            //Case 2: New shiftposition is greater than 25
+            var newShiftPosition = offset + shiftPosition;
+            if (newShiftPosition > 25)
+            {
+                newShiftPosition %= 26;
+                return Convert.ToChar(newShiftPosition + 'a');
+            }
+
+            //Case 3 : New shiftposition is less than or equal to 25
+            return Convert.ToChar(newShiftPosition + 'a');
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/StringProblems/Leetcode_848_ShiftingLetters_V1Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.StringProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.StringProblems
+{
+    [TestClass]
+    public class Leetcode_848_ShiftingLetters_V1Tests
+    {
+        [TestMethod]
+        public void Given_StringAndShiftArray_When_ShiftString_Then_ShouldReturnNewStringAfterShiftOperation()
+        {
+            //Given
+            var problem = new Leetcode_848_ShiftingLetters_V1();
+            var str = "abc";
+            var shiftArray = new int[] { 5, 6, 7 };
+            var expectedResult = "abc";
+
+            //When
+            var actualResult = problem.ShiftLetters(str, shiftArray);
+
+            //Then
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            for (var i = 0; i < expectedResult.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
