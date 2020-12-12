2c37245cd32b59640ff807cd472617098eccc01f
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2020-12-12 23:57:20 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_496_NextGreaterElementI_V1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/next-greater-element-i/
+    /// </summary>
+    public class Leetcode_496_NextGreaterElementI_V1
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            var nextGreaterElementLookup = new Dictionary<int, int>();
+            var nextGreaterElementStack = new Stack<int>();
+            nextGreaterElementStack.Push(nums2[0]);
+            for (var i = 1; i < nums2.Length; i++)
+            {
+                var currentNumber = nums2[i];
+                while (nextGreaterElementStack.Count > 0)
+                {
+                    var previousNumber = nextGreaterElementStack.Peek();
+                    if (currentNumber > previousNumber)
+                    {
+                        nextGreaterElementLookup.Add(previousNumber, currentNumber);
+                        nextGreaterElementStack.Pop();
+                    }
+                    else
+                    {
+                        nextGreaterElementStack.Push(currentNumber);
+                        break;
+                    }
+                }
+            }
+
+            var nextGreaterElementArray = new int[nums1.Length];
+            for (var i = 0; i < nums1.Length; i++)
+            {
+                if(nextGreaterElementLookup.ContainsKey(nums1[i]))
+                {
+                    nextGreaterElementArray[i] = nextGreaterElementLookup[nums1[i]];
+                }
+                else
+                {
+                    nextGreaterElementArray[i] = -1;
+                }
+            }
+
+            return nextGreaterElementArray;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_496_NextGreaterElementI_V1Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_496_NextGreaterElementI_V1Tests
+    {
+        [TestMethod]
+        public void Given_TwoIntegerArrays_When_NextGreaterElement_Then_ShouldRetrunArrayContainingNextGreaterElements()
+        {
+            //Given
+            var nums1 = new int[] { 4, 1, 2 };
+            var nums2 = new int[] { 1, 3, 4, 2};
+            var problem = new Leetcode_496_NextGreaterElementI_V1();
+            var expectedResult = new int[] { -1, 3, -1 };
+
+            //When
+            var actualResult = problem.NextGreaterElement(nums1, nums2);
+
+            //Then
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            for(var i = 0; i < expectedResult.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
