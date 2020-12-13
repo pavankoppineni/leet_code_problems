a8fbb35d2cb7c01992b82c73e58cca262d66eda5
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2020-12-13 14:13:18 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_11_ContianerWithMostWater_V1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/container-with-most-water/
+    /// </summary>
+    public class Leetcode_11_ContianerWithMostWater_V1
+    {
+        public int FindContainerWithMaxWater(int[] heights)
+        {
+            var currentMax = 0;
+            var max = 0;
+            var left = 0;
+            var right = heights.Length - 1;
+            while (left < right)
+            {
+                var leftHeight = heights[left];
+                var rightHeight = heights[right];
+                var distance = right - left;
+
+                //Case 1 : Left and right are equal
+                if (leftHeight == rightHeight)
+                {
+                    currentMax = distance * heights[left];
+                    left += 1;
+                    right -= 1;
+                }
+                //Case 2 : Left is greater than right
+                else if (leftHeight > rightHeight)
+                {
+                    currentMax = distance * heights[right];
+                    right -= 1;
+                }
+                //Case 3 : Left is less than right
+                else
+                {
+                    currentMax = distance * heights[left];
+                    left += 1;
+                }
+
+                if (currentMax > max)
+                {
+                    max = currentMax;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_11_ContianerWithMostWater_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_11_ContianerWithMostWater_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfVerticalLines_When_FindContainerWithMaximumWater_Then_ShouldReturnMaximumWaterContainerCanHold()
+        {
+            //Given
+            var problem = new Leetcode_11_ContianerWithMostWater_V1();
+            var heights = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var expectedResult = 49;
+
+            //When
+            var actualResult = problem.FindContainerWithMaxWater(heights);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
