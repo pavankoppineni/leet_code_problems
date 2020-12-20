d106595ffdbe044835d066e1002939d3dd135c99
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2020-12-20 23:19:28 +0530

    added new problem

--- test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_59_SpiralMatrixII_V1Tests.cs
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_59_SpiralMatrixII_V1Tests.cs
@@ -7,14 +7,14 @@ using System.Text;
 namespace LeetCodeProblems.Tests.MatrixProblems
 {
     [TestClass]
-    public class Leetcode_54_SpiralMatrixII_V1Tests
+    public class Leetcode_59_SpiralMatrixII_V1Tests
     {
         [TestMethod]
         public void Given_Number_When_GenerateSpiralMatrix_Then_ShouldReturnSpiralMatrix()
         {
             //Given
             var number = 2;
-            var problem = new Leetcode_54_SpiralMatrixII_V1();
+            var problem = new Leetcode_59_SpiralMatrixII_V1();
             var expectedSpiralItems = new int[2][];
             expectedSpiralItems[0] = new int[] { 1, 2 };
             expectedSpiralItems[1] = new int[] { 4, 3 };
