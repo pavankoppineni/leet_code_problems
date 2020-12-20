84404f6cfa93bbd0a50850b6cdbe8ea457412f54
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2020-12-20 23:17:27 +0530

    Added matrix proble

---
+++ src/LeetCodeProblems/MatrixProblems/Leetcode_54_SpiralMatrixII_V1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/spiral-matrix-ii/
+    /// </summary>
+    public class Leetcode_54_SpiralMatrixII_V1
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            var maxElement = n * n;
+
+            var spiralOrderItems = new int[n][];
+            var currentIndex = 0;
+            while (currentIndex < n)
+            {
+                spiralOrderItems[currentIndex] = new int[n];
+                currentIndex++;
+            }
+
+            //Row index elements
+            var startRow = 0;
+            var endRow = n;
+
+            //Column index elements
+            var startColumn = 0;
+            var endColumn = n;
+
+            var currentValue = 1;
+
+            while (currentValue <= maxElement)
+            {
+                //Traverse row from left to right
+                for (var column = startColumn; column < endColumn; column++)
+                {
+                    spiralOrderItems[startRow][column] = currentValue;
+                    currentValue += 1;
+                }
+                startRow += 1;
+
+                //Traverse column from top to bottom
+                for (var row = startRow; row < endRow; row++)
+                {
+                    spiralOrderItems[row][endColumn - 1] = currentValue;
+                    currentValue += 1;
+                }
+                endColumn -= 1;
+
+                //Traverse row from right to left
+                for (var column = endColumn - 1; column >= startColumn; column--)
+                {
+                    spiralOrderItems[endRow - 1][column] = currentValue;
+                    currentValue += 1;
+                }
+                endRow -= 1;
+
+                //Traverse column from bottom to top
+                for (var row = endRow - 1; row >= startRow; row--)
+                {
+                    spiralOrderItems[row][startColumn] = currentValue;
+                    currentValue += 1;
+                }
+                startColumn += 1;
+
+            }
+
+            return spiralOrderItems;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_54_SpiralMatrixII_V1Tests.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeProblems.MatrixProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems
+{
+    [TestClass]
+    public class Leetcode_54_SpiralMatrixII_V1Tests
+    {
+        [TestMethod]
+        public void Given_Number_When_GenerateSpiralMatrix_Then_ShouldReturnSpiralMatrix()
+        {
+            //Given
+            var number = 2;
+            var problem = new Leetcode_54_SpiralMatrixII_V1();
+            var expectedSpiralItems = new int[2][];
+            expectedSpiralItems[0] = new int[] { 1, 2 };
+            expectedSpiralItems[1] = new int[] { 4, 3 };
+
+            //When
+            var actualSpiralItems = problem.GenerateMatrix(number);
+
+            //Then
+            Assert.AreEqual(expectedSpiralItems.Length, actualSpiralItems.Length);
+            for (var row = 0; row < expectedSpiralItems.Length; row++)
+            {
+                Assert.AreEqual(expectedSpiralItems[row].Length, actualSpiralItems[row].Length);
+                for (var index = 0; index < expectedSpiralItems[row].Length; index++)
+                {
+                    Assert.AreEqual(expectedSpiralItems[row][index], actualSpiralItems[row][index]);
+                }
+            }
+        }
+    }
+}
