67118fab7369b86a0ae2122784190a78cb694092
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-01-01 22:11:24 +0530

    Added new proble

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_922_SortArrayByParityII_V1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/sort-array-by-parity-ii/
+    /// </summary>
+    public class Leetcode_922_SortArrayByParityII_V1
+    {
+        public int[] SortyByParity(int[] values)
+        {
+            var newValues = new int[values.Length];
+            var evenIndex = 0;
+            var oddIndex = 1;
+            for (var i = 0; i < values.Length; i++)
+            {
+                var val = values[i];
+                var numberType = val & 1;
+
+                //Case 1 : Value is odd
+                if (numberType == 1)
+                {
+                    newValues[oddIndex] = val;
+                    oddIndex += 2;
+                }
+                //Case 2 : Value is even
+                else
+                {
+                    newValues[evenIndex] = val;
+                    evenIndex += 2;
+                }
+            }
+            return newValues;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_922_SortArrayByParityII_V1Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_922_SortArrayByParityII_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_SortArrayByParity_Then_ShouldReturnArraySortedByParity()
+        {
+            //Given
+            var values = new int[] { 4, 10, 16, 1, 3, 9 };
+            var problem = new Leetcode_922_SortArrayByParityII_V1();
+            var expectedResult = new int[] { 4, 1, 10, 3, 16, 9 };
+
+            //When
+            var actualResult = problem.SortyByParity(values);
+
+            //Then
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
