6e35082b266ab4b07e865dc21eb4af4ca2d7edbd
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-01-28 23:12:36 +0530

    Added array nesting problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_565_ArrayNesting_V1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/array-nesting/
+    /// </summary>
+    public class Leetcode_565_ArrayNesting_V1
+    {
+        public int ArrayNesting(int[] nums)
+        {
+            var visited = new bool[nums.Length];
+            var maxCount = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var start = nums[i];
+                //Case 1 : When element is visited
+                if (visited[start])
+                {
+                    continue;
+                }
+                //Case 2 : When element is not visted
+                else
+                {
+                    var count = 0;
+                    do
+                    {
+                        visited[start] = true;
+                        count += 1;
+                        start = nums[start];
+                    }
+                    while (start != nums[i]);
+                    if(count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_565_ArrayNesting_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_565_ArrayNesting_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_ArrayNesting_Then_ShouldReturnLongestSubSetOfArrayNesting()
+        {
+            //Given
+            var values = new int[] { 5, 4, 0, 3, 1, 6, 2 };
+            var problem = new Leetcode_565_ArrayNesting_V1();
+            var expectedResult = 4;
+
+            //When
+            var actualResult = problem.ArrayNesting(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
