833aca64aeb19d858593ca8f4b97aa30121e9469
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-14 22:24:33 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_1343_NumberOfSubArraysOfSizeKAndAverageGreaterThanOr EqualToThreshold_V1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/
+    /// </summary>
+    public class Leetcode_1343_NumberOfSubArraysOfSizeKAndAverageGreaterThanOr_EqualToThreshold_V1
+    {
+        public int FindNumberOfSubArrays(int[] values, int subArraySize, int threshold)
+        {
+            var count = 0;
+            var currentSum = 0;
+            var index = 0;
+            while(index < subArraySize)
+            {
+                currentSum += values[index];
+                index += 1;
+            }
+            if (currentSum / subArraySize >= threshold)
+            {
+                count += 1;
+            }
+            while (index < values.Length)
+            {
+                currentSum += values[index];
+                currentSum -= values[index - subArraySize];
+                if (currentSum / subArraySize >= threshold)
+                {
+                    count += 1;
+                }
+                index += 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_1343_NumberOfSubArraysOfSizeKAndAverageGreaterThanOr_EqualToThreshold_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_1343_NumberOfSubArraysOfSizeKAndAverageGreaterThanOr_EqualToThreshold_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegersAndThreshold_When_FindNumberOfSubArrays_Then_ShouldReturnNumberOfSubArrays()
+        {
+            //Given
+            var values = new int[] { 2, 2, 2, 2, 5, 5, 5, 8 };
+            var subArraySize = 3;
+            var threshold = 4;
+            var expectedResult = 3;
+            var problem = new Leetcode_1343_NumberOfSubArraysOfSizeKAndAverageGreaterThanOr_EqualToThreshold_V1();
+            //When
+            var actualResult = problem.FindNumberOfSubArrays(values, subArraySize, threshold);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
