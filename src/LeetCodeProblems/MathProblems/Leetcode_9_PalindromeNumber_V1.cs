afaf3ea619600a5bbcfb43b89f23eb841551ede6
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-15 19:29:01 +0530

    Added palindrome problem

---
+++ src/LeetCodeProblems/MathProblems/Leetcode_9_PalindromeNumber_V1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MathProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/palindrome-number/
+    /// </summary>
+    public class Leetcode_9_PalindromeNumber_V1
+    {
+        public bool IsPalindrome(int number)
+        {
+            var duplicateNumber = number;
+            if (number == 0)
+            {
+                return true;
+            }
+
+            if (number < 0)
+            {
+                number *= -1;
+            }
+
+            var newNumber = 0;
+            while (number > 0)
+            {
+                var reminder = number % 10;
+                newNumber = newNumber * 10 + reminder;
+                number /= 10;
+            }
+
+            if (duplicateNumber == newNumber)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MathProblems/Leetcode_9_PalindromeNumber_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.MathProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MathProblems
+{
+    [TestClass]
+    public class Leetcode_9_PalindromeNumber_V1Tests
+    {
+        [TestMethod]
+        public void Given_Number_When_CheckPalindrome_Then_ShouldReturnTrueForPalindromeNumber()
+        {
+            //Given
+            var number = 121;
+            var problem = new Leetcode_9_PalindromeNumber_V1();
+            var expectedResult = true;
+
+            //When
+            var actualResult = problem.IsPalindrome(number);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
