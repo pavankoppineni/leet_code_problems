1c3f4106468aa25105ac7babea99f49da5f64dda
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-16 18:42:24 +0530

    added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_179_LargestNumber_V1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/largest-number/
+    /// </summary>
+    public class Leetcode_179_LargestNumber_V1
+    {
+        public string FindLargestNumber(int[] values)
+        {
+            var strings = new string[values.Length];
+            for (var index = 0; index < values.Length; index++)
+            {
+                strings[index] = values[index].ToString();
+            }
+            Array.Sort(strings, new LargestNumberComparable());
+            var result = string.Empty;
+            for (var index = strings.Length - 1; index >= 0; index--)
+            {
+                result += strings[index];
+            }
+            if (result[0] == '0')
+            {
+                return "0";
+            }
+            return result;
+        }
+
+        public class LargestNumberComparable : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return (x + y).CompareTo(y + x);
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_179_LargestNumber_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_179_LargestNumber_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_FindLargestNumber_Then_ShouldReturnLargestNumber()
+        {
+            //Given
+            var values = new int[] { 3, 30, 34, 5, 9 };
+            var expectedResult = "9534330";
+            var problem = new Leetcode_179_LargestNumber_V1();
+
+            //When
+            var actualResult = problem.FindLargestNumber(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
