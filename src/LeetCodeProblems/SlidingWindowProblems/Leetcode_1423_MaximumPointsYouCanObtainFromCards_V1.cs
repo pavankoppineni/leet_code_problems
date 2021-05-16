86cea1c37590caaef3431ed24a8a8206416a04ae
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-16 21:51:12 +0530

    Added new problem

---
+++ src/LeetCodeProblems/SlidingWindowProblems/Leetcode_1423_MaximumPointsYouCanObtainFromCards_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.SlidingWindowProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+    /// </summary>
+    public class Leetcode_1423_MaximumPointsYouCanObtainFromCards_V1
+    {
+        public int CalculateMaximumScore(int[] values, int k)
+        {
+            var maxSum = 0;
+            for (var index = 0; index < k; index++)
+            {
+                maxSum += values[index];
+            }
+
+            if (k == values.Length)
+            {
+                return maxSum;
+            }
+
+            var currentSum = maxSum;
+            var count = 1;
+            while (count <= k)
+            {
+                currentSum -= values[k - count];
+                currentSum += values[values.Length - count];
+                count += 1;
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/SlidingWindowProblems/Leetcode_1423_MaximumPointsYouCanObtainFromCards_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.SlidingWindowProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.SlidingWindowProblems
+{
+    [TestClass]
+    public class Leetcode_1423_MaximumPointsYouCanObtainFromCards_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_FindMaximumPoints_Then_ReturnMaximumPoints()
+        {
+            //Given
+            var values = new int[] { 1, 2, 3, 4, 5, 6, 1 };
+            var expectedValue = 12;
+            var problem = new Leetcode_1423_MaximumPointsYouCanObtainFromCards_V1();
+            var k = 3;
+
+            //When
+            var actualValue = problem.CalculateMaximumScore(values, k);
+
+            //Then
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
