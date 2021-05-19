dfd7fe2be52ed8045ea21b1667f49408515ef6aa
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-19 22:08:14 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_88_MergeTwoSortedArrays_V1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/merge-sorted-array/
+    /// </summary>
+    public class Leetcode_88_MergeTwoSortedArrays_V1
+    {
+        public IList<int> Merge(int[] arrayOne, int[] arrayTwo)
+        {
+            var result = new List<int>();
+            var i = 0;
+            var j = 0;
+            while (i < arrayOne.Length && j < arrayTwo.Length)
+            {
+                var valueOne = arrayOne[i];
+                var valueTwo = arrayTwo[j];
+
+                //Case 1 : When valueOne is less than valueTwo
+                //Action : 
+                if (valueOne < valueTwo)
+                {
+                    result.Add(valueOne);
+                    i += 1;
+                }
+                //Case 2 : When valueOne is greater than valueTwo
+                //Action :
+                else if (valueOne > valueTwo)
+                {
+                    result.Add(valueTwo);
+                    j += 1;
+                }
+                //Case 3 : When valueOne and valueTwo are equal
+                //Action : 
+                else
+                {
+                    result.Add(valueTwo);
+                    result.Add(valueOne);
+                    i += 1;
+                    j += 1;
+                }
+            }
+
+            while (i < arrayOne.Length)
+            {
+                result.Add(arrayOne[i]);
+                i += 1;
+            }
+
+            while (j < arrayTwo.Length)
+            {
+                result.Add(arrayTwo[j]);
+                j += 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_88_MergeTwoSortedArrays_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_88_MergeTwoSortedArrays_V1Tests
+    {
+        [TestMethod]
+        public void Given_TwoSortedArrays_When_Merge_Then_ShouldReturnSortedArray()
+        {
+            //Given
+            var arrayOne = new int[] { 1, 2, 3 };
+            var arrayTwo = new int[] { 2, 5, 6 };
+            var problem = new Leetcode_88_MergeTwoSortedArrays_V1();
+            var expectedResult = new int[] { 1, 2, 2, 3, 5, 6 };
+
+            //When
+            var actualResult = problem.Merge(arrayOne, arrayTwo);
+
+            //Then
+            Assert.AreEqual(expectedResult.Length, actualResult.Count);
+        }
+    }
+}
