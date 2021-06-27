a721a7c324fc34a24e26c53d5feb6ea418b90795
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-06-28 00:16:26 +0530

    added combination sum problem

--- src/LeetCodeProblems/BacktrackingProblems/Leetcode_39_CombinationSum_V1.cs
+++ src/LeetCodeProblems/BacktrackingProblems/Leetcode_39_CombinationSum_V1.cs
@@ -9,9 +9,37 @@ namespace LeetCodeProblems.BacktrackingProblems
     /// </summary>
     public class Leetcode_39_CombinationSum_V1
     {
+        private int[] values;
+        private IList<IList<int>> combinations;
+
         public IList<IList<int>> FindCombinations(int[] values, int sum)
         {
-            throw new NotImplementedException();
+            combinations = new List<IList<int>>();
+            this.values = values;
+            var combination = new Stack<int>();
+            FindCombinations(0, combination, sum);
+            return combinations;
+        }
+
+        private void FindCombinations(int index, Stack<int> combination, int sum)
+        {
+            if(sum == 0)
+            {
+                this.combinations.Add(combination.ToArray());
+                return;
+            }
+
+            if (sum < 0)
+            {
+                return;
+            }
+
+            for (var currentIndex = index; currentIndex < values.Length; currentIndex++)
+            {
+                combination.Push(values[currentIndex]);
+                FindCombinations(currentIndex, combination, sum - values[currentIndex]);
+                combination.Pop();
+            }
         }
     }
 }
---
+++ test/LeetCodeProblems.Tests/BacktrackingProblems/Leetcode_39_CombinationSum_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.BacktrackingProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BacktrackingProblems
+{
+    [TestClass]
+    public class Leetcode_39_CombinationSum_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_GenerateCombinationSum_Then_ShouldReturnListOfCombinations()
+        {
+            //Given
+            var values = new int[] { 2, 3, 6, 7 };
+            var problem = new Leetcode_39_CombinationSum_V1();
+            var expectedCombinationsCount = 2;
+            var sum = 7;
+
+            //When
+            var actualCombinations = problem.FindCombinations(values, sum);
+
+            //Then
+            Assert.AreEqual(expectedCombinationsCount, actualCombinations.Count);
+        }
+    }
+}
