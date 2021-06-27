5498e373a1736faa1fddf41728ecfecbbc30c65a
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-06-27 23:50:31 +0530

    added subsets

---
+++ src/LeetCodeProblems/BacktrackingProblems/Leetcode_78_Subsets_V2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BacktrackingProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subsets/
+    /// </summary>
+    public class Leetcode_78_Subsets_V2
+    {
+        private IList<IList<int>> subsets;
+        private int[] values;
+        public IList<IList<int>> GenerateSubsets(int[] values)
+        {
+            this.values = values;
+            subsets = new List<IList<int>>();
+            var subset = new Stack<int>();
+            GenerateSubsets(0, subset);
+            return subsets;
+        }
+
+        private void GenerateSubsets(int index, Stack<int> stack)
+        {
+            if (index == values.Length)
+            {
+                subsets.Add(stack.ToArray());
+                return;
+            }
+            GenerateSubsets(index + 1, stack);
+            stack.Push(values[index]);
+            GenerateSubsets(index + 1, stack);
+            stack.Pop();
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BacktrackingProblems/Leetcode_78_Subsets_V2Tests.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeProblems.BacktrackingProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BacktrackingProblems
+{
+    [TestClass]
+    public class Leetcode_78_Subsets_V2Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_GenerateSubsets_Then_ShouldGenerateSubsets()
+        {
+            //Given
+            var values = new int[] { 1, 2, 3 };
+            var problem = new Leetcode_78_Subsets_V2();
+            var expectedSubsets = new List<IList<int>>
+            {
+                new List<int>{ },
+                new List<int>{ 1 },
+                new List<int>{ 2 },
+                new List<int>{ 3 },
+                new List<int>{ 1, 2 },
+                new List<int>{ 1, 3 },
+                new List<int>{ 2, 3 },
+                new List<int>{ 1, 2, 3 },
+            };
+
+            //When
+            var actualSubsets = problem.GenerateSubsets(values);
+
+            //Then
+            Assert.AreEqual(expectedSubsets.Count, actualSubsets.Count);
+        }
+    }
+}
