ee9721d8a373c3e5d92f0073954f85cb49a6232b
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-07-28 10:26:23 +0530

    Added new problem

---
+++ src/LeetCodeProblems/TreeProblems/Leetcode_104_MaximumDepthOfBinaryTree_V2.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems.TreeProblems.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.TreeProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    public class Leetcode_104_MaximumDepthOfBinaryTree_V2
+    {
+        public int FindMaximumDepth(TreeNode treeNode)
+        {
+            if (treeNode == null)
+            {
+                return 0;
+            }
+
+            var leftTreeDepth = FindMaximumDepth(treeNode.Left);
+            var rightTreeDepth = FindMaximumDepth(treeNode.Right);
+            return 1 + Math.Max(leftTreeDepth, rightTreeDepth);
+        }
+    }
+}
---
+++ src/LeetCodeProblems/TreeProblems/Leetcode_111_MinimumDepthOfBinaryTree_V1.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems.TreeProblems.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.TreeProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+    /// </summary>
+    public class Leetcode_111_MinimumDepthOfBinaryTree_V1
+    {
+        public int MinDepth(TreeNode treeNode)
+        {
+            if (treeNode == null)
+            {
+                return 0;
+            }
+
+            var leftTreeDepth = MinDepth(treeNode.Left);
+            var rightTreeDepth = MinDepth(treeNode.Right);
+            return 1 + Math.Min(leftTreeDepth, rightTreeDepth);
+        }
+    }
+}
