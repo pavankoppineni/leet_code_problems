5898af28dc9203883fd4db407920962a824ca9fb
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-08-02 21:32:30 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_287_FindTheDuplicateNumber_V3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-the-duplicate-number/
+    /// </summary>
+    public class Leetcode_287_FindTheDuplicateNumber_V3
+    {
+        public int FindDuplicateNumber(int[] values)
+        {
+            var duplicateNumber = 0;
+            for (var index = 0; index < values.Length; index++)
+            {
+                var itemIndex = Math.Abs(values[index]);
+                var indexValue = values[itemIndex];
+                if (indexValue < 0)
+                {
+                    duplicateNumber = itemIndex;
+                    break;
+                }
+                else
+                {
+                    values[itemIndex] = -1 * values[itemIndex];
+                }
+            }
+            return duplicateNumber;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_287_FindTheDuplicateNumber_V3Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_287_FindTheDuplicateNumber_V3Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_FindDuplicateNumber_Then_ShoulReturnDuplicateNumber()
+        {
+            //Given
+            var values = new int[] { 3, 1, 3, 4, 2 };
+            var problem = new Leetcode_287_FindTheDuplicateNumber_V3();
+            var expectedResult = 3;
+
+            //When
+            var actualResult = problem.FindDuplicateNumber(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
