3a08c466073f3b542cc218827b152ca028a7f8a7
pkoppineni <pkoppineni@csod.com>
2021-08-30 13:24:08 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_442_FindAllDuplicatesInArray_V1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    /// </summary>
+    public class Leetcode_442_FindAllDuplicatesInArray_V1
+    {
+        public IList<int> FindDuplicates(int[] values)
+        {
+            var duplicates = new HashSet<int>();
+            for (var index = 0; index < values.Length; index++)
+            {
+                var value = Math.Abs(values[index]);
+                var duplicateValue = values[value - 1];
+                if (duplicateValue < 0)
+                {
+                    duplicates.Add(value);
+                }
+                else
+                {
+                    values[value - 1] = -1 * values[value - 1];
+                }
+            }
+            return duplicates.ToList();
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_442_FindAllDuplicatesInArray_V1Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_442_FindAllDuplicatesInArray_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_FindDuplicates_Then_ShouldReturnDuplicates()
+        {
+            //Given
+            var values = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            var expectedResult = new HashSet<int> { 2, 3 };
+            var problem = new Leetcode_442_FindAllDuplicatesInArray_V1();
+
+            //When
+            var actualResult = problem.FindDuplicates(values);
+
+            //Then
+            foreach (var actualItem in actualResult)
+            {
+                var expected = expectedResult.Contains(actualItem);
+                Assert.AreEqual(true, expected);
+            }
+        }
+    }
+}
