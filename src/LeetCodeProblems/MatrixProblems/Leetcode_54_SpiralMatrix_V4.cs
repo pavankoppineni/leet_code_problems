f3340029faa5141b5046f4051bd6c43c6393023e
pkoppineni <pkoppineni@csod.com>
2021-08-31 08:27:10 +0530

    Added new problem

---
+++ src/LeetCodeProblems/MatrixProblems/Leetcode_54_SpiralMatrix_V4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/spiral-matrix/
+    /// </summary>
+    public class Leetcode_54_SpiralMatrix_V4
+    {
+        public IList<int> SpiralOrder(int[,] matrix)
+        {
+            var items = new List<int>();
+            var rows = matrix.GetLength(0);
+            var columns = matrix.GetLength(1);
+            var startRow = 0;
+            var endRow = rows;
+            var startColumn = 0;
+            var endColumn = columns;
+            while (true)
+            {
+                if (startRow >= endRow || startColumn >= endColumn)
+                {
+                    break;
+                }
+
+                //Traverse row from left to right
+                if (startRow < endRow)
+                {
+                    for (var column = startColumn; column < endColumn; column++)
+                    {
+                        items.Add(matrix[startRow, column]);
+                    }
+                    startRow += 1;
+                }
+
+                //Traverse column from top to bottom
+                if (startColumn < endColumn)
+                {
+                    for (var row = startRow; row < endRow; row++)
+                    {
+                        items.Add(matrix[row, endColumn - 1]);
+                    }
+                    endColumn -= 1;
+                }
+
+                //Traverse row from right to left
+                if (startRow < endRow)
+                {
+                    for (var column = endColumn - 1; column >= startColumn; column--)
+                    {
+                        items.Add(matrix[endRow - 1, column]);
+                    }
+                    endRow -= 1;
+                }
+
+                //Traverse column from bottom to top
+                if (startColumn < endColumn)
+                {
+                    for (var row = endRow - 1; row >= startRow; row--)
+                    {
+                        items.Add(matrix[row, startColumn]);
+                    }
+                    startColumn += 1;
+                }
+            }
+
+            return items;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_54_SpiralMatrix_V4Tests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeProblems.MatrixProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems
+{
+    [TestClass]
+    public class Leetcode_54_SpiralMatrix_V4Tests
+    {
+        [TestMethod]
+        public void Given_Matrix_When_TraverseSpiral_Then_ShouldReturnItemsInSpiralOrder()
+        {
+            //Given
+            var matrix = new int[3, 4]
+            {
+                {1, 2, 3, -1 },
+                {4, 5, 6, -2 },
+                {7, 8, 9, -3 }
+            };
+            var problem = new Leetcode_54_SpiralMatrix_V4();
+            var expectedOrder = new int[] { 1, 2, 3, -1, -2, -3, 9, 8, 7, 4, 5, 6 };
+
+            //When
+            var actualOrder = problem.SpiralOrder(matrix);
+
+            //Then
+            for (var index = 0; index < expectedOrder.Length; index++)
+            {
+                Assert.AreEqual(expectedOrder[index], actualOrder[index]);
+            }
+        }
+    }
+}
