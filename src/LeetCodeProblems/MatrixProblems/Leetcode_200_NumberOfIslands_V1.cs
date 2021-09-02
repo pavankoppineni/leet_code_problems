a76d02cfd9e09112bc83f5f18fab66a2de0f2999
pkoppineni <pkoppineni@csod.com>
2021-09-02 22:20:27 +0530

    Added new problem

---
+++ src/LeetCodeProblems/MatrixProblems/Leetcode_200_NumberOfIslands_V1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-islands/
+    /// </summary>
+    public class Leetcode_200_NumberOfIslands_V1
+    {
+        private int[][] directions = new int[4][];
+        public Leetcode_200_NumberOfIslands_V1()
+        {
+            directions[0] = new int[] { 0, 1 };
+            directions[1] = new int[] { 0, -1 };
+            directions[2] = new int[] { 1, 0 };
+            directions[3] = new int[] { -1, 0 };
+        }
+
+        public int FindNumberOfIslands(int[,] matrix)
+        {
+            var count = 0;
+            var rows = matrix.GetLength(0);
+            var columns = matrix.GetLength(1);
+            var visited = new bool[rows, columns];
+            for (var row = 0; row < rows; row++)
+            {
+                for (var column = 0; column < columns; column++)
+                {
+                    if (visited[row, column])
+                    {
+                        continue;
+                    }
+                    var cell = matrix[row, column];
+                    visited[row, column] = true;
+                    if (cell == 0)
+                    {
+                        continue;
+                    }
+                    CountIslandArea(row, column, rows, columns, visited, matrix);
+                    count += 1;
+                }
+            }
+            return count;
+        }
+
+        private void CountIslandArea(int row, int column, int rows, int columns, bool[,] visited, int[,] matrix)
+        {
+            foreach (var direction in directions)
+            {
+                var currentRow = row + direction[0];
+                var currentColumn = column + direction[1];
+                if (currentRow >= rows || currentColumn >= columns || currentRow < 0 || currentColumn < 0)
+                {
+                    continue;
+                }
+                if (visited[currentRow, currentColumn])
+                {
+                    continue;
+                }
+                visited[currentRow, currentColumn] = true;
+                if (matrix[currentRow, currentColumn] == 0)
+                {
+                    continue;
+                }
+                CountIslandArea(currentRow, currentColumn, rows, columns, visited, matrix);
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_200_NumberOfIslands_V1Tests.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeProblems.MatrixProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems
+{
+    [TestClass]
+    public class Leetcode_200_NumberOfIslands_V1Tests
+    {
+        [TestMethod]
+        public void Given_Matrix_When_FindNumberOfIslands_Then_ShouldReturnNumberOfIslands()
+        {
+            //Given
+            var matrix = new int[4, 4]
+            {
+                {1, 0, 0, 1 },
+                {1, 0, 0, 1 },
+                {1, 1, 1, 0 },
+                {1, 1, 1, 0 }
+            };
+            var expectedIslandCount = 2;
+            var problem = new Leetcode_200_NumberOfIslands_V1();
+
+            //When
+            var actualIslandCount = problem.FindNumberOfIslands(matrix);
+
+            //Then
+            Assert.AreEqual(expectedIslandCount, actualIslandCount);
+        }
+    }
+}
