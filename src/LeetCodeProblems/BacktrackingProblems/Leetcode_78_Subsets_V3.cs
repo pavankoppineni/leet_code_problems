e9718bb9ea12e2d7f24b25036727d2fc5ffe2a6e
pkoppineni <pkoppineni@csod.com>
2021-09-08 19:14:55 +0530

    Added new problem

---
+++ src/LeetCodeProblems/BacktrackingProblems/Leetcode_78_Subsets_V3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BacktrackingProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subsets/
+    /// </summary>
+    public class Leetcode_78_Subsets_V3
+    {
+        private Stack<int> _currentStack;
+        private int[] _values;
+        private IList<IList<int>> _subsets;
+        public IList<IList<int>> GenerateSubsets(int[] values)
+        {
+            _currentStack = new Stack<int>();
+            _values = values;
+            _subsets = new List<IList<int>>();
+            GenerateSubsets(0);
+            return _subsets;
+        }
+
+        private void GenerateSubsets(int index)
+        {
+            if (index == _values.Length)
+            {
+                _subsets.Add(_currentStack.ToArray());
+                return;
+            }
+            GenerateSubsets(index + 1);
+            _currentStack.Push(_values[index]);
+            GenerateSubsets(index + 1);
+            _currentStack.Pop();
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BacktrackingProblems/Leetcode_78_Subsets_V3Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.BacktrackingProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BacktrackingProblems
+{
+    [TestClass]
+    public class Leetcode_78_Subsets_V3Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_GenerateSubsets_Then_ShouldGenerateSubsets()
+        {
+            //Given
+            var values = new int[] { 1, 2, 3 };
+            var problem = new Leetcode_78_Subsets_V3();
+            var expectedSubsets = 8;
+
+            //When
+            var actualSubsets = problem.GenerateSubsets(values);
+
+            //Then
+            Assert.AreEqual(expectedSubsets, actualSubsets.Count);
+        }
+    }
+}
