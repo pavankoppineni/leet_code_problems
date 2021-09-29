bf8fb1d3018355f90d9e7e6ad9206257eb0f6bf8
pkoppineni <pkoppineni@csod.com>
2021-09-29 10:01:59 +0530

    Added new problems

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_560_SubarraySumEqualsK_V2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subarray-sum-equals-k/
+    /// </summary>
+    public class Leetcode_560_SubarraySumEqualsK_V2
+    {
+        public int FintTotalSubArrayCount(int[] values, int sum)
+        {
+            var prefixSum = new int[values.Length];
+            var prefixSumHashSet = new Dictionary<int, IList<int>>();
+            var subArrayCount = 0;
+            prefixSum[0] = values[0];
+            for (var index = 1; index < values.Length; index++)
+            {
+                prefixSum[index] = values[index] + prefixSum[index - 1];
+            }
+
+            for (var index = 0; index < values.Length; index++)
+            {
+                if (prefixSumHashSet.ContainsKey(prefixSum[index]))
+                {
+                    prefixSumHashSet[prefixSum[index]].Add(index);
+                }
+                else
+                {
+                    prefixSumHashSet.Add(prefixSum[index], new List<int> { index });
+                }
+            }
+
+            for (var index = 0; index < prefixSum.Length; index++)
+            {
+                var value = prefixSum[index];
+                var valueWithoutSum = value - sum;
+                if (prefixSumHashSet.ContainsKey(valueWithoutSum))
+                {
+                    subArrayCount += prefixSumHashSet[valueWithoutSum].Count;
+                }
+            }
+
+            return subArrayCount;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_560_SubarraySumEqualsK_V2Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems
+{
+    [TestClass]
+    public class Leetcode_560_SubarraySumEqualsK_V2Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_FindNumberOfSubarrays_Then_ShouldReturnNumberOfSubarrays()
+        {
+            //Given
+            var values = new int[] { 23, 2, 4, 6, 7 };
+            var problem = new Leetcode_560_SubarraySumEqualsK_V2();
+            var expectedResult = 2;
+            var sum = 6;
+
+            //When
+            var actualResult = problem.FintTotalSubArrayCount(values, sum);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
