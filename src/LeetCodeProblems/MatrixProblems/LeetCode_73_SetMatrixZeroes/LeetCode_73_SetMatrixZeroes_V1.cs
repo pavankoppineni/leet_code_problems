754046d3c54df9046dfc7531ccee823d3229e604
pkoppineni <pkoppineni@csod.com>
2021-12-06 19:19:13 +0530

    Added new problem

---
+++ src/LeetCodeProblems/MatrixProblems/LeetCode_73_SetMatrixZeroes/LeetCode_73_SetMatrixZeroes_V1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems.LeetCode_73_SetMatrixZeroes
+{
+    /// <summary>
+    /// https://leetcode.com/problems/set-matrix-zeroes/
+    /// </summary>
+    public class LeetCode_73_SetMatrixZeroes_V1
+    {
+        public int[][] SetZeroes(int[][] matrix)
+        {
+            var rowsLength = matrix.Length;
+            var colsLength = matrix[0].Length;
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                for (var col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == 0)
+                    {
+                        matrix[0][col] = 0;
+                        matrix[row][0] = 0;
+                    }
+                }
+            }
+
+            //Set rows
+            for (var row = 0; row < rowsLength; row++)
+            {
+                if (matrix[row][0] != 0)
+                {
+                    continue;
+                }
+
+                for (var col = 0; col < colsLength; col++)
+                {
+                    matrix[row][col] = 0;
+                }
+            }
+
+            //Set columns
+            for (var col = 0; col < colsLength; col++)
+            {
+                if (matrix[0][col] != 0)
+                {
+                    continue;
+                }
+
+                for (var row = 0; row < rowsLength; row++)
+                {
+                    matrix[row][col] = 0;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/LeetCode_73_SetMatrixZeroes/LeetCode_73_SetMatrixZeroes_V1Tests.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeProblems.MatrixProblems.LeetCode_73_SetMatrixZeroes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems.LeetCode_73_SetMatrixZeroes
+{
+    [TestClass]
+    public class LeetCode_73_SetMatrixZeroes_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerMatrix_When_SetZeros_Then_ShouldReturnMatrixAfterSettingZeros()
+        {
+            //Given
+            var matrix = new int[][]
+            {
+                new int[] { 1, 1, 1 },
+                new int[] { 1, 0, 1 },
+                new int[] { 1, 1, 1 }
+            };
+            var problem = new LeetCode_73_SetMatrixZeroes_V1();
+            var expectedMatrix = new int[][]
+            {
+                new int[] { 1, 0, 1 },
+                new int[] { 0, 0, 0 },
+                new int[] { 1, 0, 1 }
+            };
+
+            //When
+            var actualMatrix = problem.SetZeroes(matrix);
+
+            //Then
+            for(var row = 0; row < matrix.Length; row++)
+            {
+                for (var column = 0; column < matrix[row].Length; column++)
+                {
+                    Assert.AreEqual(expectedMatrix[row][column], actualMatrix[row][column]);
+                }
+            }
+        }
+    }
+}
