5aeda87ca6572eaad7fb808c977b3b1db16447bd
pkoppineni <pkoppineni@csod.com>
2021-12-15 23:10:17 +0530

    Added new dynamic programming problem

---
+++ src/LeetCodeProblems/DynamicProgramming/Leetcode_746_MinCostClimbingStairs/Leetcode_746_MinCostClimbingStairs_V1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming.Leetcode_746_MinCostClimbingStairs
+{
+    /// <summary>
+    /// https://leetcode.com/problems/min-cost-climbing-stairs/
+    /// </summary>
+    public class Leetcode_746_MinCostClimbingStairs_V1
+    {
+        public int MinCostClimbingStairs(int[] costs)
+        {
+            var firstStep = MinCostClimbingStairs(costs, 0);
+            var secondStep = MinCostClimbingStairs(costs, 1);
+            return Math.Min(firstStep, secondStep);
+        }
+
+        private int MinCostClimbingStairs(int[] costs, int index)
+        {
+            if (index >= costs.Length)
+            {
+                return 0;
+            }
+            var firstStep = MinCostClimbingStairs(costs, index + 1);
+            var secondStep = MinCostClimbingStairs(costs, index + 2);
+            return costs[index] + Math.Min(firstStep, secondStep);
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/DynamicProgramming/Leetcode_746_MinCostClimbingStairs/Leetcode_746_MinCostClimbingStairs_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.DynamicProgramming.Leetcode_746_MinCostClimbingStairs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.DynamicProgramming.Leetcode_746_MinCostClimbingStairs
+{
+    [TestClass]
+    public class Leetcode_746_MinCostClimbingStairs_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_CalculateMinCostOfClimbingStairs_Then_ShouldReturnMinConst()
+        {
+            //Given
+            var costs = new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+            var problem = new Leetcode_746_MinCostClimbingStairs_V1();
+            var expectedCost = 6;
+
+            //When
+            var actualCost = problem.MinCostClimbingStairs(costs);
+
+            //Then
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+    }
+}
