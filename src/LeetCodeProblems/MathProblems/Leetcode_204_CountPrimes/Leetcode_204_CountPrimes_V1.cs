b4035539c77ea8a60d885bcdc47d6baa8bf028f2
pkoppineni <pkoppineni@csod.com>
2021-12-15 22:16:06 +0530

    Added new problem

---
+++ src/LeetCodeProblems/MathProblems/Leetcode_204_CountPrimes/Leetcode_204_CountPrimes_V1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MathProblems.Leetcode_204_CountPrimes
+{
+    /// <summary>
+    /// https://leetcode.com/problems/count-primes/
+    /// </summary>
+    public class Leetcode_204_CountPrimes_V1
+    {
+        public int CountPrimes(int n)
+        {
+            var validPrimes = new bool[n + 1];
+            for (var i = 2; i <= n; i++)
+            {
+                if (!validPrimes[i])
+                {
+                    for (var j = i * i; j <= n; j += i)
+                    {
+                        validPrimes[j] = true;
+                    }
+                }
+            }
+            var count = 0;
+            for (var i = 2; i <= n; i++)
+            {
+                if (!validPrimes[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MathProblems/Leetcode_204_CountPrimes/Leetcode_204_CountPrimes_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.MathProblems;
+using LeetCodeProblems.MathProblems.Leetcode_204_CountPrimes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MathProblems.Leetcode_204_CountPrimes
+{
+    [TestClass]
+    public class Leetcode_204_CountPrimes_V1Tests
+    {
+        [TestMethod]
+        public void Given_Integer_When_CountPrimes_Then_ShouldReturnNumberOfPrimes()
+        {
+            //Given
+            var n = 15;
+            var problem = new Leetcode_204_CountPrimes_V1();
+            var expectedResult = 6;
+
+            //When
+            var actualResult = problem.CountPrimes(n);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
