d0c561ebf21390a20b2d31490dd58afa703acc5a
pkoppineni <pkoppineni@csod.com>
2021-12-16 18:00:10 +0530

    Added new problem

---
+++ src/LeetCodeProblems/DynamicProgramming/Leetcode_64_MinimumPathSum/Leetcode_64_MinimumPathSum_Recursion_V1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming.Leetcode_64_MinimumPathSum
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-path-sum/
+    /// </summary>
+    public class Leetcode_64_MinimumPathSum_Recursion_V1
+    {
+        public int FindMinimumPathSum(int[][] maze)
+        {
+            return FindMinimumPathSum(maze, maze.Length - 1, maze[0].Length - 1);
+        }
+
+        private int FindMinimumPathSum(int[][] maze, int row, int column)
+        {
+            if (row == 0 && column == 0)
+            {
+                return maze[0][0];
+            }
+
+            var isValidTop = IsValid(maze, row - 1, column);
+            var isValidLeft = IsValid(maze, row, column - 1);
+            if (isValidTop && isValidLeft)
+            {
+                return maze[row][column] + Math.Min(FindMinimumPathSum(maze, row - 1, column), FindMinimumPathSum(maze, row, column - 1));
+            }
+            else if (isValidTop)
+            {
+                return maze[row][column] + FindMinimumPathSum(maze, row - 1, column);
+            }
+            return maze[row][column] + FindMinimumPathSum(maze, row, column - 1);
+        }
+
+        private bool IsValid(int[][] maze, int row, int column)
+        {
+            if (row < 0 || column < 0)
+            {
+                return false;
+            }
+
+            if (row >= maze.Length || column >= maze[0].Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/DynamicProgramming/Leetcode_64_MinimumPathSum/Leetcode_64_MinimumPathSum_Recursion_V1Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.DynamicProgramming.Leetcode_64_MinimumPathSum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.DynamicProgramming.Leetcode_64_MinimumPathSum
+{
+    [TestClass]
+    public class Leetcode_64_MinimumPathSum_Recursion_V1Tests
+    {
+        [TestMethod]
+        public void Given_Maze_When_FindMinimumPathSum_Then_ShouldReturnMinimumPathSum()
+        {
+            //Given
+            var maze = new int[3][]
+            {
+                new int[]{1},
+                new int[]{1},
+                new int[]{1},
+            };
+            var problem = new Leetcode_64_MinimumPathSum_Recursion_V1();
+
+            //When
+            var actualResult = problem.FindMinimumPathSum(maze);
+
+            //Then
+            Assert.Inconclusive();
+        }
+    }
+}
