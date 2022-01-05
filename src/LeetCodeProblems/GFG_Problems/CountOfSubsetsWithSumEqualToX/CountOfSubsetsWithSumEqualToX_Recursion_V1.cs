ed8fa921c24316e45fe6009baabd1aea6ea34ff9
pkoppineni <pkoppineni@csod.com>
2022-01-05 12:11:39 +0530

    Added new gfg problem

---
+++ src/LeetCodeProblems/GFG_Problems/CountOfSubsetsWithSumEqualToX/CountOfSubsetsWithSumEqualToX_Recursion_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.GFG_Problems.CountOfSubsetsWithSumEqualToX
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/count-of-subsets-with-sum-equal-to-x/
+    /// </summary>
+    public class CountOfSubsetsWithSumEqualToX_Recursion_V1
+    {
+        private int[] _arr;
+        public int CountOfSubsetsWithSumEqualToX(int[] arr, int sum)
+        {
+            _arr = arr;
+            return CountOfSubsetsWithSumEqualToX(0, sum);
+        }
+
+        private int CountOfSubsetsWithSumEqualToX(int level, int sum)
+        {
+            if (sum == 0)
+            {
+                return 1;
+            }
+
+            if (level >= _arr.Length)
+            {
+                return 0;
+            }
+
+            if (sum < 0)
+            {
+                return 0;
+            }
+
+            var left = CountOfSubsetsWithSumEqualToX(level + 1, sum - _arr[level]);
+            var right = CountOfSubsetsWithSumEqualToX(level + 1, sum);
+            return left + right;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/GFG_Problems/CountOfSubsetsWithSumEqualToX/CountOfSubsetsWithSumEqualToX_Recursion_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.GFG_Problems.CountOfSubsetsWithSumEqualToX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.GFG_Problems.CountOfSubsetsWithSumEqualToX
+{
+    [TestClass]
+    public class CountOfSubsetsWithSumEqualToX_Recursion_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_CountOfSubsetsWithSumEqualToX_Then_ShouldReturnCount()
+        {
+            //Given
+            var arr = new int[] { 1, 2, 3, 3 };
+            var problem = new CountOfSubsetsWithSumEqualToX_Recursion_V1();
+            var expectedCount = 3;
+            var sum = 6;
+
+            //When
+            var actualCount = problem.CountOfSubsetsWithSumEqualToX(arr, 6);
+
+            //Then
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
