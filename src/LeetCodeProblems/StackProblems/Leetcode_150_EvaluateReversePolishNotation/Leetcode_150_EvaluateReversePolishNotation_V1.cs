153374bc0e8e57698ff76c56baff96a6e3c3bcdc
pkoppineni <pkoppineni@csod.com>
2022-01-05 15:03:34 +0530

    Added new problem

---
+++ src/LeetCodeProblems/StackProblems/Leetcode_150_EvaluateReversePolishNotation/Leetcode_150_EvaluateReversePolishNotation_V1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.StackProblems.Leetcode_150_EvaluateReversePolishNotation
+{
+    /// <summary>
+    /// https://leetcode.com/problems/evaluate-reverse-polish-notation/
+    /// </summary>
+    public class Leetcode_150_EvaluateReversePolishNotation_V1
+    {
+        public int Evaluate(string[] tokens)
+        {
+            var stack = new Stack<int>();
+            for (var index = 0; index < tokens.Length; index++)
+            {
+                var isOperator = IsOperator(tokens[index]);
+                if (isOperator)
+                {
+                    var rightOperand = stack.Pop();
+                    var leftOperand = stack.Pop();
+                    var result = Evaluate(leftOperand, rightOperand, tokens[index]);
+                    stack.Push(result);
+                }
+                else
+                {
+                    stack.Push(int.Parse(tokens[index]));
+                }
+            }
+            return stack.Pop();
+        }
+
+        private bool IsOperator(string token)
+        {
+            switch (token)
+            {
+                case "-":
+                case "+":
+                case "*":
+                case "/":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private int Evaluate(int left, int right, string token)
+        {
+            switch (token)
+            {
+                case "-":
+                    return left - right;
+                case "+":
+                    return left + right;
+                case "*":
+                    return left * right;
+                default:
+                    return left / right;
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/StackProblems/Leetcode_150_EvaluateReversePolishNotation/Leetcode_150_EvaluateReversePolishNotation_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.StackProblems.Leetcode_150_EvaluateReversePolishNotation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.StackProblems.Leetcode_150_EvaluateReversePolishNotation
+{
+    [TestClass]
+    public class Leetcode_150_EvaluateReversePolishNotation_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfStrings_When_EvaluateTokens_Then_ShouldReturnValue()
+        {
+            //Given
+            var tokens = new string[] { "2", "1", "+", "3", "*" };
+            var problem = new Leetcode_150_EvaluateReversePolishNotation_V1();
+            var expectedResult = 9;
+
+            //When
+            var actualResult = problem.Evaluate(tokens);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
