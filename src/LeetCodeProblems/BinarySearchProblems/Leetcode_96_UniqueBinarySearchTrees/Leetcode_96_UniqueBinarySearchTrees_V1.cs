289b24e217c13e52f2638fe0919d9e8f02ada77e
pkoppineni <pkoppineni@csod.com>
2022-01-05 16:34:38 +0530

    Added new problem

---
+++ src/LeetCodeProblems/BinarySearchProblems/Leetcode_96_UniqueBinarySearchTrees/Leetcode_96_UniqueBinarySearchTrees_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BinarySearchProblems.Leetcode_96_UniqueBinarySearchTrees
+{
+    /// <summary>
+    /// https://leetcode.com/problems/unique-binary-search-trees/
+    /// </summary>
+    public class Leetcode_96_UniqueBinarySearchTrees_V1
+    {
+        private IDictionary<int, int> _table;
+        public int CreateUniqueBinarySearchTrees(int number)
+        {
+            _table = new Dictionary<int, int>();
+            return CreateUniqueBinarySearchTrees(1, number);
+        }
+
+        private int CreateUniqueBinarySearchTrees(int startExclusive, int endExclusive)
+        {
+            if (endExclusive < startExclusive)
+            {
+                return 1;
+            }
+            var diff = endExclusive - startExclusive;
+            if (_table.ContainsKey(diff))
+            {
+                return _table[diff];
+            }
+            var nodes = 0;
+            for (int i = startExclusive; i <= endExclusive; i++)
+            {
+                var leftNodes = CreateUniqueBinarySearchTrees(startExclusive, i - 1);
+                var rightNodes = CreateUniqueBinarySearchTrees(i + 1, endExclusive);
+                nodes += leftNodes * rightNodes;
+            }
+            _table.Add(diff, nodes);
+            return nodes;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BinarySearchProblems/Leetcode_96_UniqueBinarySearchTrees/Leetcode_96_UniqueBinarySearchTrees_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.BinarySearchProblems.Leetcode_96_UniqueBinarySearchTrees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BinarySearchProblems.Leetcode_96_UniqueBinarySearchTrees
+{
+    [TestClass]
+    public class Leetcode_96_UniqueBinarySearchTrees_V1Tests
+    {
+        [TestMethod]
+        public void Given_Number_When_CountUniqueBinaryTrees_Then_ShouldReturnCount()
+        {
+            //Given
+            var number = 18;
+            var problem = new Leetcode_96_UniqueBinarySearchTrees_V1();
+            var expectedResult = 477638700;
+
+            //When
+            var actualResult = problem.CreateUniqueBinarySearchTrees(number);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
