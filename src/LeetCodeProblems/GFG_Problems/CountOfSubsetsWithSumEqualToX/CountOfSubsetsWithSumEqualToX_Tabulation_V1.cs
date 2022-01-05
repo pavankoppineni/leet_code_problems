132005e501bdf139d7c1874de5dd08932d911784
pkoppineni <pkoppineni@csod.com>
2022-01-05 14:43:20 +0530

    Added new probelm

---
+++ src/LeetCodeProblems/GFG_Problems/CountOfSubsetsWithSumEqualToX/CountOfSubsetsWithSumEqualToX_Tabulation_V1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.GFG_Problems.CountOfSubsetsWithSumEqualToX
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/count-of-subsets-with-sum-equal-to-x/
+    /// </summary>
+    public class CountOfSubsetsWithSumEqualToX_Tabulation_V1
+    {
+        public int CountOfSubsetsWithSumEqualToX(int[] arr, int sum)
+        {
+            var rows = arr.Length + 1;
+            var columns = sum + 1;
+            var table = new int[rows, columns];
+            table[0, 0] = 1;
+            for(var row = 1; row < rows; row++)
+            {
+                var item = arr[row];
+                for (var col = 0; col < columns; col++)
+                {
+                    //Case 1 : When item is greater than col
+                    //Action : Get value from previous row and same column
+                    if (item > col)
+                    {
+                        table[row, col] = table[row - 1, col];
+                    }
+                    //Case 2 : When item is less than or equal to col
+                    //Action : Get count from following places
+                    //1. Include item
+                    //2. Exclude item
+                    else
+                    {
+                        var includeItemCount = table[row - 1, col - item];
+                        var excludeItemCount = table[row - 1, col];
+                        table[row, col] = includeItemCount + excludeItemCount;
+                    }
+                }
+            }
+
+            return table[rows - 1, columns - 1];
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/GFG_Problems/CountOfSubsetsWithSumEqualToX/CountOfSubsetsWithSumEqualToX_Tabulation_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.GFG_Problems.CountOfSubsetsWithSumEqualToX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.GFG_Problems.CountOfSubsetsWithSumEqualToX
+{
+    [TestClass]
+    public class CountOfSubsetsWithSumEqualToX_Tabulation_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_CountSubsetsWithSumEqualToX_Then_ShouldReturnCount()
+        {
+            //Given
+            var arr = new int[] { 1, 2, 3, 3 };
+            var problem = new CountOfSubsetsWithSumEqualToX_Tabulation_V1();
+            var expectedCount = 3;
+            var sum = 6;
+
+            //When
+            var actualCount = problem.CountOfSubsetsWithSumEqualToX(arr, 6);
+
+            //Then
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
