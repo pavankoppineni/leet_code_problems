e9babd57ae7c11ccb2fe6e8f365a1e25dbe9e9a1
pkoppineni <pkoppineni@csod.com>
2022-01-06 22:27:23 +0530

    Added new problems

---
+++ src/LeetCodeProblems/BinarySearchProblems/Leetcode_95_UniqueBinarySearchTrees/Leetcode_95_UniqueBinarySearchTrees_V1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BinarySearchProblems.Leetcode_95_UniqueBinarySearchTrees
+{
+    /// <summary>
+    /// https://leetcode.com/problems/unique-binary-search-trees-ii/
+    /// </summary>
+    public class Leetcode_95_UniqueBinarySearchTrees_V1
+    {
+        public IList<Node95> CreateUniqueBinarySearchTrees(int number)
+        {
+            return CreateUniqueBinarySearchTrees(1, number);
+        }
+
+        private IList<Node95> CreateUniqueBinarySearchTrees(int startExclusive, int endExclusive)
+        {
+            if (endExclusive < startExclusive)
+            {
+                return null;
+            }
+            var nodes = new List<Node95>();
+            for (int i = startExclusive; i <= endExclusive; i++)
+            {
+                var leftNodes = CreateUniqueBinarySearchTrees(startExclusive, i - 1);
+                var rightNodes = CreateUniqueBinarySearchTrees(i + 1, endExclusive);
+            }
+            return nodes;
+        }
+    }
+
+    public class Node95
+    {
+        public int Value { get; set; }
+        public Node95 Left { get; set; }
+        public Node95 Right { get; set; }
+    }
+}
---
+++ src/LeetCodeProblems/TreeProblems/Leetcode_662_MaximumWidthOfBinaryTree/Leetcode_662_MaximumWidthOfBinaryTree_V1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.TreeProblems.Leetcode_662_MaximumWidthOfBinaryTree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-width-of-binary-tree/
+    /// </summary>
+    public class Leetcode_662_MaximumWidthOfBinaryTree_V1
+    {
+        IDictionary<int, (int min, int max)> _levels;
+        public int WidthOfBinaryTree(Node root)
+        {
+            _levels = new Dictionary<int, (int min, int max)>();
+            WidthOfBinaryTree(root, 0, 1);
+            int maxWidth = 1;
+            foreach (var level in _levels.Values)
+            {
+                var currentWidth = Math.Abs(level.max - level.min) + 1;
+                if (currentWidth > maxWidth)
+                {
+                    maxWidth = currentWidth;
+                }
+            }
+            return maxWidth;
+        }
+
+        public void WidthOfBinaryTree(Node root, int level, int width)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (_levels.ContainsKey(level))
+            {
+                (var min, var max) = _levels[level];
+                if (width < min)
+                {
+                    _levels[level] = (width, max);
+                }
+                else if (width > max)
+                {
+                    _levels[level] = (min, width);
+                }
+            }
+            else
+            {
+                _levels.Add(level, (width, width));
+            }
+            WidthOfBinaryTree(root.right, level + 1, 2 * width);
+            WidthOfBinaryTree(root.left, level + 1, 2 * width + 1);
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node right;
+        public Node left;
+    }
+}
