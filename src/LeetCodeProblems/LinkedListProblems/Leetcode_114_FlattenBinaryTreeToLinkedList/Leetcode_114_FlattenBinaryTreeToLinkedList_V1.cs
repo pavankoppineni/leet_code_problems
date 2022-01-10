3f3977e09a10269428ace594715a6a02aa70312b
pkoppineni <pkoppineni@csod.com>
2022-01-10 22:32:42 +0530

    Added new problem

---
+++ src/LeetCodeProblems/LinkedListProblems/Leetcode_114_FlattenBinaryTreeToLinkedList/Leetcode_114_FlattenBinaryTreeToLinkedList_V1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.LinkedListProblems.Leetcode_114_FlattenBinaryTreeToLinkedList
+{
+    /// <summary>
+    /// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+    /// </summary>
+    public class Leetcode_114_FlattenBinaryTreeToLinkedList_V1
+    {
+        public TreeNode Flatten(TreeNode treeNode)
+        {
+            PreOrderTraversal(treeNode);
+            return treeNode;
+        }
+
+        private TreeNode PreOrderTraversal(TreeNode node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            var leftNode = PreOrderTraversal(node.left);
+            var rightNode = PreOrderTraversal(node.right);
+            var currentNode = node;
+            while (leftNode != null)
+            {
+                currentNode.right = leftNode;
+                currentNode = leftNode;
+                leftNode = leftNode.right;
+            }
+            while (rightNode != null)
+            {
+                currentNode.right = rightNode;
+                currentNode = rightNode;
+                rightNode = rightNode.right;
+            }
+            node.left = null;
+            return node;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+}
---
+++ test/LeetCodeProblems.Tests/LinkedListProblems/Leetcode_114_FlattenBinaryTreeToLinkedList/Leetcode_114_FlattenBinaryTreeToLinkedList_V1Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.LinkedListProblems.Leetcode_114_FlattenBinaryTreeToLinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.LinkedListProblems.Leetcode_114_FlattenBinaryTreeToLinkedList
+{
+    [TestClass]
+    public class Leetcode_114_FlattenBinaryTreeToLinkedList_V1Tests
+    {
+        [TestMethod]
+        public void Given_Tree_When_Flatten_Then_ShouldReturnLinkedList()
+        {
+            //Given
+            var treeNode = new TreeNode { val = 1 };
+            var left = new TreeNode { val = 2 };
+            var right = new TreeNode { val = 3};
+            treeNode.left = left;
+            treeNode.right = right;
+            var problem = new Leetcode_114_FlattenBinaryTreeToLinkedList_V1();
+            var expectedResult = new TreeNode { val = 1 };
+
+            //When
+            var actualResult = problem.Flatten(treeNode);
+
+            //Then
+            Assert.Inconclusive();
+        }
+    }
+}
