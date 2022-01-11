0803e76acf458adf50731bf20991891b2b59e864
pkoppineni <pkoppineni@csod.com>
2022-01-11 12:24:42 +0530

    Added new problems

---
+++ src/LeetCodeProblems/PriorityQueue/Leetcode_1338_ReduceArraySizeToHalf/Leetcode_1338_ReduceArraySizeToHalf_V1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.PriorityQueue.Leetcode_1338_ReduceArraySizeToHalf
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reduce-array-size-to-the-half/
+    /// </summary>
+    public class Leetcode_1338_ReduceArraySizeToHalf_V1
+    {
+        //JAVA Solution
+
+        //public int minSetSize(int[] arr)
+        //{
+        //    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        //    for (int i = 0; i < arr.length; i++)
+        //    {
+        //        if (map.containsKey(arr[i]))
+        //        {
+        //            Integer value = map.get(arr[i]);
+        //            map.put(arr[i], value + 1);
+        //        }
+        //        else
+        //        {
+        //            map.put(arr[i], 1);
+        //        }
+        //    }
+        //    PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>((a, b)-> {
+        //        return b - a;
+        //    });
+        //    Iterator<Map.Entry<Integer, Integer>> itr = map.entrySet().iterator();
+        
+        //    while(itr.hasNext())
+        //    {
+        //         Map.Entry<Integer, Integer> entry = itr.next();
+        //         priorityQueue.offer(entry.getValue());
+        //    }
+
+        //    Integer count = 0;
+        //    Integer removedItems = 0;
+        //    Integer maxItemsToRemove = arr.length >> 1;
+        //        while(removedItems<maxItemsToRemove)
+        //        {   
+        //            removedItems = removedItems + priorityQueue.poll();
+        //            count = count + 1;
+        //        }
+        //    return count;
+        //}
+    }
+}
---
+++ src/LeetCodeProblems/PriorityQueue/Leetcode_1845_SeatReservationManager/Leetcode_1845_SeatReservationManager_V1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.PriorityQueue.Leetcode_1845_SeatReservationManager
+{
+    /// <summary>
+    /// https://leetcode.com/problems/seat-reservation-manager/
+    /// </summary>
+    public class SeatManager
+    {
+        //JAVA Solution
+
+        //PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();
+        //public SeatManager(int n)
+        //{
+        //    for (Integer index = 1; index <= n; index++)
+        //    {
+        //        priorityQueue.offer(index);
+        //    }
+        //}
+
+        //public int reserve()
+        //{
+        //    return priorityQueue.poll();
+        //}
+
+        //public void unreserve(int seatNumber)
+        //{
+        //    priorityQueue.offer(seatNumber);
+        //}
+    }
+}
