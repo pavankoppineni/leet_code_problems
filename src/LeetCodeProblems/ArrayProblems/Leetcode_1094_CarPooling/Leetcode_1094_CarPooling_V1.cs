bc423ed6af3e25db049f57d2f53e4e5dd79bfc31
pkoppineni <pkoppineni@csod.com>
2022-01-11 12:03:33 +0530

    Added car pooling problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_1094_CarPooling/Leetcode_1094_CarPooling_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_1094_CarPooling
+{
+    /// <summary>
+    /// https://leetcode.com/problems/car-pooling/
+    /// </summary>
+    public class Leetcode_1094_CarPooling_V1
+    {
+        public bool CarPooling(int[][] trips, int capacity)
+        {
+            var result = true;
+            var min = 0;
+            var max = 0;
+            foreach (var trip in trips)
+            {
+                min = Math.Min(min, trip[1]);
+                max = Math.Max(max, trip[2]);
+            }
+            var capacityTable = new int[max + 1];
+            foreach (var trip in trips)
+            {
+                capacityTable[trip[1]] += trip[0];
+                capacityTable[trip[2]] -= trip[0];
+            }
+            var previous = 0;
+            for (var i = 0; i < capacityTable.Length; i++)
+            {
+                previous += capacityTable[i];
+                if (previous > capacity)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_1094_CarPooling/Leetcode_1094_CarPooling_V1.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_1094_CarPooling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_1094_CarPooling
+{
+    [TestClass]
+    public class Leetcode_1094_CarPooling_V1Tests
+    {
+        [TestMethod]
+        public void Given_Trips_When_CarPooling_Then_ShouldReturnTrueIfDriverCanTakeAllPassengers()
+        {
+            //Given
+            var trips = new int[2][]
+            {
+                new int[3]{9,0,1},
+                new int[3]{ 3, 3, 7 }
+            };
+            var capacity = 4;
+            var expectedResult = false;
+            var problem = new Leetcode_1094_CarPooling_V1();
+
+            //When
+            var actualResult = problem.CarPooling(trips, capacity);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
