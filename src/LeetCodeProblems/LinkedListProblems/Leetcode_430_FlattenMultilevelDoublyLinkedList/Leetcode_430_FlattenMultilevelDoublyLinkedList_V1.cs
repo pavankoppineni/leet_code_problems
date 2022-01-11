feab37fb1db86f850eeb5a41cf27a054c348b81d
pkoppineni <pkoppineni@csod.com>
2022-01-11 18:09:46 +0530

    Added new problem

---
+++ src/LeetCodeProblems/LinkedListProblems/Leetcode_430_FlattenMultilevelDoublyLinkedList/Leetcode_430_FlattenMultilevelDoublyLinkedList_V1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.LinkedListProblems.Leetcode_430_FlattenMultilevelDoublyLinkedList
+{
+    /// <summary>
+    /// https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+    /// </summary>
+    public class Leetcode_430_FlattenMultilevelDoublyLinkedList_V1
+    {
+        public Node Flatten(Node node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            if (node.next == null && node.child == null)
+            {
+                return node;
+            }
+            var currentNode = node;
+            var childNode = node.child;
+            var nextNode = node.next;
+            var flattenChildNode = Flatten(currentNode.child);
+            var flattenNextNode = Flatten(currentNode.next);
+            while (flattenChildNode != null)
+            {
+                currentNode.next = flattenChildNode;
+                flattenChildNode.prev = currentNode;
+                currentNode = flattenChildNode;
+                flattenChildNode = flattenChildNode.next;
+            }
+            while (flattenNextNode != null)
+            {
+                currentNode.next = flattenNextNode;
+                flattenNextNode.prev = currentNode;
+                currentNode = flattenNextNode;
+                flattenNextNode = flattenNextNode.next;
+            }
+            node.child = null;
+            return node;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node prev;
+        public Node child;
+    }
+}
