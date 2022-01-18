f1b40733187830b722815598c487dfc3362f65d4
pkoppineni <pkoppineni@csod.com>
2022-01-18 15:45:35 +0530

    Added leetcode 40 problem

---
+++ src/LeetCodeProblems/BacktrackingProblems/Leetcode_40_CombinationSum_II/Leetcode_40_CombinationSum_II_V1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeProblems.BacktrackingProblems.Leetcode_40_CombinationSum_II
+{
+    /// <summary>
+    /// https://leetcode.com/problems/combination-sum-ii/
+    /// </summary>
+    public class Leetcode_40_CombinationSum_II_V1
+    {
+        private IDictionary<int, int> _table;
+        private IList<IList<int>> _items;
+        private Stack<int> _stack;
+        private int[] _keys;
+        private int[] _countArray;
+        private int _length;
+        public IList<IList<int>> GenerateCombinations(int[] candidates, int target)
+        {
+            _stack = new Stack<int>();
+            _items = new List<IList<int>>();
+            _table = new Dictionary<int, int>();
+            foreach (var candidate in candidates)
+            {
+                if (_table.ContainsKey(candidate))
+                {
+                    _table[candidate] += 1;
+                }
+                else
+                {
+                    _table.Add(candidate, 1);
+                }
+            }
+            _keys = _table.Keys.ToArray();
+            _countArray = _table.Values.ToArray();
+            _length = _keys.Length;
+            GenerateCombinations(0, target);
+            return _items;
+        }
+
+        private void GenerateCombinations(int index, int target)
+        {
+            if (index >= _length)
+            {
+                return;
+            }
+            if (target == 0)
+            {
+                _items.Add(_stack.ToArray());
+                return;
+            }
+            if (target < 0)
+            {
+                return;
+            }
+            var count = _countArray[index];
+            GenerateCombinations(index + 1, target);
+
+            if (count > 0)
+            {
+                var value = _keys[index];
+                _countArray[index] -= 1;
+                _stack.Push(value);
+                GenerateCombinations(index, target - value);
+                _countArray[index] += 1;
+                _stack.Pop();
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BacktrackingProblems/Leetcode_40_CombinationSum_II/Leetcode_40_CombinationSum_II_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems.BacktrackingProblems.Leetcode_40_CombinationSum_II;
+
+namespace LeetCodeProblems.Tests.BacktrackingProblems.Leetcode_40_CombinationSum_II
+{
+    [TestClass]
+    public class Leetcode_40_CombinationSum_II_V1Tests
+    {
+        [TestMethod]
+        public void Given_CandidatesAndTarget_When_GenerateCombinations_Then_ShouldGenerateCombinations()
+        {
+            //Given
+            var candidates = new int[] { 10, 1, 2, 7, 6, 1, 5 };
+            var target = 8;
+            var problem = new Leetcode_40_CombinationSum_II_V1();
+
+            //When
+            var actualResult = problem.GenerateCombinations(candidates, target);
+
+            //Then
+            Assert.Inconclusive();
+        }
+    }
+}
