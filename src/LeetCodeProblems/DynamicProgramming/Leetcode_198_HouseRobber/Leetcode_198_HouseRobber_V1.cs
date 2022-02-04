a5cd33102e96ba37d05c3e2dee574fce61948a6d
pkoppineni <pkoppineni@csod.com>
2022-02-04 22:20:44 +0530

    Added house robber problem

---
+++ src/LeetCodeProblems/DynamicProgramming/Leetcode_198_HouseRobber/Leetcode_198_HouseRobber_V1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming.Leetcode_198_HouseRobber
+{
+    /// <summary>
+    /// https://leetcode.com/problems/house-robber/
+    /// </summary>
+    public class Leetcode_198_HouseRobber_V1
+    {
+        public int Rob(int[] houses)
+        {
+            var exclusive = 0;
+            var inclusive = houses[0];
+            for (var index = 1; index < houses.Length; index++)
+            {
+                var tempInclusive = inclusive - houses[index - 1];
+                var newInclusive = Math.Max(tempInclusive + houses[index], exclusive + houses[index]);
+                exclusive = inclusive;
+                inclusive = newInclusive;
+            }
+            return Math.Max(exclusive, inclusive);
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/DynamicProgramming/Leetcode_198_HouseRobber/Leetcode_198_HouseRobber_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.DynamicProgramming.Leetcode_198_HouseRobber;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.DynamicProgramming.Leetcode_198_HouseRobber
+{
+    [TestClass]
+    public class Leetcode_198_HouseRobber_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Rob_Then_ShouldReturnMaximumAmountThatCanBeRobbed()
+        {
+            //Given
+            var houses = new int[] { 2, 7, 9, 3, 1 };
+            var problem = new Leetcode_198_HouseRobber_V1();
+            var expectedResult = 12;
+
+            //When
+            var actualResult = problem.Rob(houses);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
