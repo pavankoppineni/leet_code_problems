eb123cd9d6f115e363b57e50e56eb9feab3a90b6
pkoppineni <pkoppineni@csod.com>
2022-02-17 16:58:55 +0530

    Added leetcode 437

---
+++ src/LeetCodeProblems/TreeProblems/Leetcode_437_PathSumIII/Leetcode_437_PathSumIII_V1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.TreeProblems.Leetcode_437_PathSumIII
+{
+    /// <summary>
+    /// https://leetcode.com/problems/path-sum-iii/
+    /// </summary>
+    public class Leetcode_437_PathSumIII_V1
+    {
+        private IDictionary<int, int> _table;
+        private int _sum;
+        private int _pathSumCount;
+        public int PathSum(TreeNode node, int sum)
+        {
+            _table = new Dictionary<int, int>();
+            _sum = sum;
+            CountPathSum(node, 0);
+            return _pathSumCount;
+        }
+
+        private void CountPathSum(TreeNode node, int pathSum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            var newSum = node.val + pathSum;
+            if (newSum == _sum)
+            {
+                _pathSumCount += 1;
+            }
+            var diff = newSum - _sum;
+            if (_table.ContainsKey(diff))
+            {
+                _pathSumCount += _table[diff];
+            }
+
+            if (_table.ContainsKey(newSum))
+            {
+                _table[newSum]++;
+            }
+            else
+            {
+                _table.Add(newSum, 1);
+            }
+
+            //Traverse left
+            CountPathSum(node.left, node.val + pathSum);
+            //Traverse right
+            CountPathSum(node.right, node.val + pathSum);
+
+            //Backtracking step
+            if (_table[newSum] > 1)
+            {
+                _table[newSum] -= 1;
+            }
+            else
+            {
+                _table.Remove(newSum);
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/TreeProblems/Leetcode_437_PathSumIII/Leetcode_437_PathSumIII_V1Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.TreeProblems.Leetcode_437_PathSumIII;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.TreeProblems.Leetcode_437_PathSumIII
+{
+    [TestClass]
+    public class Leetcode_437_PathSumIII_V1Tests
+    {
+        [TestMethod]
+        public void Given_TreeAndPathSum_When_FindCount_Then_ShouldReturnCountOfPathsWithGivenSum()
+        {
+            //Given
+            var problem = new Leetcode_437_PathSumIII_V1();
+            var node21 = new TreeNode(1);
+            var node22 = new TreeNode(1);
+            var root = new TreeNode(0);
+            root.left = node21;
+            root.right = node22;
+            var pathSum = 1;
+            var expectedCount = 4;
+
+            //When
+            var actualCount = problem.PathSum(root, pathSum);
+
+            //Then
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
