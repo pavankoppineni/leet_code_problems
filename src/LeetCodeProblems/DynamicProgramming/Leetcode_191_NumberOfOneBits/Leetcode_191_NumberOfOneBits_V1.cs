a5b10732e2590daffc439d01291c12ab956f24af
pkoppineni <pkoppineni@csod.com>
2022-03-08 15:21:44 +0530

    Added new problem

---
+++ src/LeetCodeProblems/DynamicProgramming/Leetcode_191_NumberOfOneBits/Leetcode_191_NumberOfOneBits_V1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.DynamicProgramming.Leetcode_191_NumberOfOneBits
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-1-bits/
+    /// </summary>
+    public class Leetcode_191_NumberOfOneBits_V1
+    {
+        public int CalculateHammingWeight(uint number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+            //Case 1 : Number is odd
+            if ((number & 1) == 1)
+            {
+                return CalculateHammingWeight(number >> 1) + 1;
+            }
+            //Case 2 : Number is even
+            return CalculateHammingWeight(number >> 1);
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/DynamicProgramming/Leetcode_191_NumberOfOneBits/Leetcode_191_NumberOfOneBits_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.DynamicProgramming.Leetcode_191_NumberOfOneBits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.DynamicProgramming.Leetcode_191_NumberOfOneBits
+{
+    [TestClass]
+    public class Leetcode_191_NumberOfOneBits_V1Tests
+    {
+        [TestMethod]
+        public void Given_Number_When_CalculateHammingWeight_Then_ShouldReturnNumberOfSetBits()
+        {
+            //Given
+            uint number = 8;
+            var problem = new Leetcode_191_NumberOfOneBits_V1();
+            var expectedResult = 1;
+
+            //When
+            var actualResult = problem.CalculateHammingWeight(number);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
