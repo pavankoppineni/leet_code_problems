f13fcf436461e4621dd12ca36a65aaf2fdfa6f1b
pkoppineni <pkoppineni@csod.com>
2022-03-14 18:42:36 +0530

    Added new problem

--- src/LeetCodeProblems/BFS/Leetcode_994_RottingOranges/Leetcode_994_RottingOranges_V1.cs
+++ src/LeetCodeProblems/BFS/Leetcode_994_RottingOranges/Leetcode_994_RottingOranges_V1.cs
@@ -9,24 +9,88 @@ namespace LeetCodeProblems.BFS.Leetcode_994_RottingOranges
     /// </summary>
     public class Leetcode_994_RottingOranges_V1
     {
-        private int[,] _directions;
+        private int[][] _directions;
+        private bool[,] _visited;
+        private int[,] _time;
         public Leetcode_994_RottingOranges_V1()
         {
-            _directions = new int[,]
+            _directions = new int[][]
             {
-                {0,1 },
-                {0, -1 },
-                {1, 0 },
-                {-1, 0 },
+                new int[]{0,1 },
+                new int[]{0, -1 },
+                new int[]{1, 0 },
+                new int[]{-1, 0 },
             };
         }
 
         public int CalculateMinimumMinutesToRot(int[][] grid)
         {
+            var maxTime = 0;
             var rows = grid.Length;
             var cols = grid[0].Length;
+            _visited = new bool[rows, cols];
+            _time = new int[rows, cols];
             var queue = new Queue<(int row, int col)>();
-            return 0;
+            for (var row = 0; row < rows; row++)
+            {
+                for (var col = 0; col < cols; col++)
+                {
+                    if (grid[row][col] == 2)
+                    {
+                        _time[row, col] = 0;
+                        _visited[row, col] = true;
+                        queue.Enqueue((row, col));
+                    }
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                var item = queue.Dequeue();
+                foreach (var direction in _directions)
+                {
+                    var newRow = item.row + direction[0];
+                    var newCol = item.col + direction[1];
+                    if (newCol >= cols || newCol < 0)
+                    {
+                        continue;
+                    }
+                    if (newRow >= rows || newRow < 0)
+                    {
+                        continue;
+                    }
+                    if (_visited[newRow, newCol])
+                    {
+                        continue;
+                    }
+                    if (grid[newRow][newCol] == 0)
+                    {
+                        _visited[newRow, newCol] = true;
+                        continue;
+                    }
+                    _time[newRow, newCol] = _time[item.row, item.col] + 1;
+                    maxTime = Math.Max(maxTime, _time[newRow, newCol]);
+                    queue.Enqueue((newRow, newCol));
+                    _visited[newRow, newCol] = true;
+                }
+            }
+
+            for (var row = 0; row < rows; row++)
+            {
+                for (var col = 0; col < cols; col++)
+                {
+                    if (grid[row][col] == 1)
+                    {
+                        if (_time[row, col] == 0)
+                        {
+                            maxTime = -1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return maxTime;
         }
     }
 }
---
+++ test/LeetCodeProblems.Tests/BFS/Leetcode_994_RottingOranges/Leetcode_994_RottingOranges_V1Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.BFS.Leetcode_994_RottingOranges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BFS.Leetcode_994_RottingOranges
+{
+    [TestClass]
+    public class Leetcode_994_RottingOranges_V1Tests
+    {
+        [TestMethod]
+        public void Given_OrangeGrid_When_CalculateMinTime_Then_ShouldReturnMinTimeToRot()
+        {
+            //Given
+            var grid = new int[][]
+            {
+                new int[]{ 2,1,1},
+                new int[]{1,1,0},
+                new int[]{0,1,1}
+            };
+            var problem = new Leetcode_994_RottingOranges_V1();
+            var expectedMinTime = 4;
+            //When
+            var actualMinTime = problem.CalculateMinimumMinutesToRot(grid);
+
+            //Then
+            Assert.AreEqual(expectedMinTime, actualMinTime);
+        }
+    }
+}
