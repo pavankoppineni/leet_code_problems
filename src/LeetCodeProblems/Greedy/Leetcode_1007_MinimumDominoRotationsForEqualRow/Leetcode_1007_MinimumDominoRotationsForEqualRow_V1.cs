dca6dc909a2139d9d6fc17fa0b32af9cfd0997ff
pkoppineni <pkoppineni@csod.com>
2022-03-21 19:01:19 +0530

    Added new problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_1007_MinimumDominoRotationsForEqualRow/Leetcode_1007_MinimumDominoRotationsForEqualRow_V1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Greedy.Leetcode_1007_MinimumDominoRotationsForEqualRow
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+    /// </summary>
+    public class Leetcode_1007_MinimumDominoRotationsForEqualRow_V1
+    {
+        public int MinDominoRotations(int[] tops, int[] bottoms)
+        {
+            var topCount = Calculate(tops, bottoms, tops[0]);
+            var bottomsCount = Calculate(tops, bottoms, bottoms[0]);
+            if (topCount == -1)
+            {
+                return bottomsCount;
+            }
+            if (bottomsCount == -1)
+            {
+                return topCount;
+            }
+            return Math.Min(topCount, bottomsCount);
+        }
+
+        private int Calculate(int[] top, int[] bottom, int value)
+        {
+            var topCount = 0;
+            var bottomCount = 0;
+            for (var index = 0; index < top.Length; index++)
+            {
+                if (top[index] != value && bottom[index] != value)
+                {
+                    return -1;
+                }
+
+                if (top[index] != value)
+                {
+                    topCount++;
+                }
+
+                if (bottom[index] != value)
+                {
+                    bottomCount++;
+                }
+            }
+
+            return Math.Min(topCount, bottomCount);
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_1007_MinimumDominoRotationsForEqualRow/Leetcode_1007_MinimumDominoRotationsForEqualRow_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_1007_MinimumDominoRotationsForEqualRow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_1007_MinimumDominoRotationsForEqualRow
+{
+    [TestClass]
+    public class Leetcode_1007_MinimumDominoRotationsForEqualRow_V1Tests
+    {
+        [TestMethod]
+        public void Given_TopAndBottomArray_When_FindMinimumRotations_Then_ShouldReturnMinRotations()
+        {
+            //Given
+            var top = new int[] { 2, 1, 2, 4, 2, 2};
+            var bottom = new int[] { 5, 2, 6, 2, 3, 2 };
+            var problem = new Leetcode_1007_MinimumDominoRotationsForEqualRow_V1();
+            var expectedResult = 2;
+
+            //When
+            var actualResult = problem.MinDominoRotations(top, bottom);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
