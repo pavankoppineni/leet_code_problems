e2d2b24ea400ca08307bd04fdae33600c27f6c06
pkoppineni <pkoppineni@csod.com>
2022-03-25 16:56:10 +0530

    Added new greedy problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_881_BoatsToSavePeople/Leetcode_881_BoatsToSavePeople_V1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Greedy.Leetcode_881_BoatsToSavePeople
+{
+    /// <summary>
+    /// https://leetcode.com/problems/boats-to-save-people/
+    /// </summary>
+    public class Leetcode_881_BoatsToSavePeople_V1
+    {
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            if (people[0] > limit)
+            {
+                return 0;
+            }
+
+            var left = 0;
+            var right = people.Length - 1;
+            var count = 0;
+            while (left <= right)
+            {
+                if (people[right] > limit)
+                {
+                    right--;
+                }
+                else if (people[left] + people[right] <= limit)
+                {
+                    left++;
+                    right--;
+                    count += 1;
+                }
+                else
+                {
+                    right--;
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_881_BoatsToSavePeople/Leetcode_881_BoatsToSavePeople_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_881_BoatsToSavePeople;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_881_BoatsToSavePeople
+{
+    [TestClass]
+    public class Leetcode_881_BoatsToSavePeople_V1Tests
+    {
+        [TestMethod]
+        public void Given_PeopleWithHeightsAndBoatLimit_When_FindMinimumNumberOfBoats_Then_ShouldReturnMinimumNumberOfBoats()
+        {
+            //Given
+            var people = new int[] { 3, 2, 2, 1 };
+            var limit = 3;
+            var expectedResult = 3;
+            var problem = new Leetcode_881_BoatsToSavePeople_V1();
+
+            //When
+            var actualResult = problem.NumRescueBoats(people, limit);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
