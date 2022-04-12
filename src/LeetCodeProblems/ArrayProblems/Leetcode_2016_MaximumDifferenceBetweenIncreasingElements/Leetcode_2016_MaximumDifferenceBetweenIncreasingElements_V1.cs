6de24ce09eb5f139f6fd8be8ef2a915d306bea72
pkoppineni <pkoppineni@csod.com>
2022-04-12 14:15:02 +0530

    Added leetcode 2016

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_2016_MaximumDifferenceBetweenIncreasingElements/Leetcode_2016_MaximumDifferenceBetweenIncreasingElements_V1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_2016_MaximumDifferenceBetweenIncreasingElements
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+    /// </summary>
+    public class Leetcode_2016_MaximumDifferenceBetweenIncreasingElements_V1
+    {
+        public int CalculateMaximumDifference(int[] nums)
+        {
+            int maxDifference = 0;
+            var min = nums[0];
+            for (var index = 1; index < nums.Length; index++)
+            {
+                if (nums[index] < min)
+                {
+                    min = nums[index];
+                    continue;
+                }
+                var currentDifference = nums[index] - min;
+                maxDifference = Math.Max(maxDifference, currentDifference);
+            }
+            return maxDifference <= 0 ? -1 : maxDifference;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_2016_MaximumDifferenceBetweenIncreasingElements/Leetcode_2016_MaximumDifferenceBetweenIncreasingElements_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_2016_MaximumDifferenceBetweenIncreasingElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_2016_MaximumDifferenceBetweenIncreasingElements
+{
+    [TestClass]
+    public class Leetcode_2016_MaximumDifferenceBetweenIncreasingElements_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_CalculateMaxDifference_Then_ShouldReturnMaxDifference()
+        {
+            //Given
+            var nums = new int[] { 1, 2, 3, 4 };
+            var problem = new Leetcode_2016_MaximumDifferenceBetweenIncreasingElements_V1();
+            var expectedResult = 3;
+
+            //When
+            var actualResult = problem.CalculateMaximumDifference(nums);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
