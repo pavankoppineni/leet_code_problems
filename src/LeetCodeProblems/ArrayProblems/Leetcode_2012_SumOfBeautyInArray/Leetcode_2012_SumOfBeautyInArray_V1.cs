4a6edce83d2b751ca6aff3a2b7125d3823ce8102
pkoppineni <pkoppineni@csod.com>
2022-04-12 12:12:25 +0530

    Added new leetcode problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_2012_SumOfBeautyInArray/Leetcode_2012_SumOfBeautyInArray_V1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_2012_SumOfBeautyInArray
+{
+    /// <summary>
+    /// https://leetcode.com/problems/sum-of-beauty-in-the-array/
+    /// </summary>
+    public class Leetcode_2012_SumOfBeautyInArray_V1
+    {
+        public int CalculateSumOfBeauties(int[] nums)
+        {
+            var prefixArray = new int[nums.Length];
+            var suffixArray = new int[nums.Length];
+
+            prefixArray[0] = nums[0];
+            for (var index = 1; index < nums.Length; index++)
+            {
+                prefixArray[index] = Math.Max(prefixArray[index - 1], nums[index]);
+            }
+
+            suffixArray[nums.Length - 1] = nums[nums.Length - 1];
+            for (var index = nums.Length - 2; index >= 0; index--)
+            {
+                suffixArray[index] = Math.Min(suffixArray[index + 1], nums[index]);
+            }
+
+            var sum = 0;
+            for (var index = 1; index < nums.Length - 1; index++)
+            {
+                var value = nums[index];
+                //Case 1 : Value is greater than all items to the left and value is less than all items to right
+                if (prefixArray[index - 1] < value && suffixArray[index + 1] > value)
+                {
+                    sum += 2;
+                    continue;
+                }
+
+                //Case 2 : Value is greater than index-1 and less than index+1
+                if (value > nums[index - 1] && value < nums[index + 1])
+                {
+                    sum += 1;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_2012_SumOfBeautyInArray/Leetcode_2012_SumOfBeautyInArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_2012_SumOfBeautyInArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_2012_SumOfBeautyInArray
+{
+    [TestClass]
+    public class Leetcode_2012_SumOfBeautyInArrayTests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_CalculateSumOfBeauty_Then_ShouldReturnSum()
+        {
+            //Given
+            var nums = new int[] { 9, 9, 3, 8, 7, 9, 6, 10 };
+            var problem = new Leetcode_2012_SumOfBeautyInArray_V1();
+            var expectedResult = 4;
+
+            //When
+            var actualResult = problem.CalculateSumOfBeauties(nums);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
