ad6676ebd17f2f0da32a54dc1a9e3ae4a1281b4e
pkoppineni <pkoppineni@csod.com>
2022-04-24 18:40:49 +0530

    Added new problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_2078_TwoFurthestHousesWithDifferentColors/Leetcode_2078_TwoFurthestHousesWithDifferentColors_V1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Greedy.Leetcode_2078_TwoFurthestHousesWithDifferentColors
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-furthest-houses-with-different-colors/
+    /// </summary>
+    public class Leetcode_2078_TwoFurthestHousesWithDifferentColors_V1
+    {
+        public int CalculateMaximumDistance(int[] colors)
+        {
+            var firstColor = colors[0];
+            var lastColor = colors[colors.Length - 1];
+            var maxDistanceLeft = 0;
+            var maxDistanceRight = 0;
+            for (var index = colors.Length - 1; index >= 0; index--)
+            {
+                if (colors[index] != firstColor)
+                {
+                    maxDistanceLeft = index;
+                    break;
+                }
+            }
+
+            for (var index = 0; index < colors.Length; index++)
+            {
+                if (colors[index] != lastColor)
+                {
+                    maxDistanceRight = colors.Length - index - 1;
+                    break;
+                }
+            }
+
+            return Math.Max(maxDistanceLeft, maxDistanceRight);
+        }
+    }
+
+    public  class House
+    {
+        public int Color { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+        public static House Create(int color, int index)
+        {
+            return new House { Color = color, Start = index, End = index };
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_2078_TwoFurthestHousesWithDifferentColors/Leetcode_2078_TwoFurthestHousesWithDifferentColors_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_2078_TwoFurthestHousesWithDifferentColors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_2078_TwoFurthestHousesWithDifferentColors
+{
+    [TestClass]
+    public class Leetcode_2078_TwoFurthestHousesWithDifferentColors_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_FindFarthestHouses_Then_ShouldCalcualteFarthestHouses()
+        {
+            //Given
+            var houses = new int[] { 4, 4, 4, 11, 4, 4, 11, 4, 4, 4, 4, 4 };
+            var problem = new Leetcode_2078_TwoFurthestHousesWithDifferentColors_V1();
+            var expectedResult = 8;
+
+            //When
+            var actualResult = problem.CalculateMaximumDistance(houses);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
