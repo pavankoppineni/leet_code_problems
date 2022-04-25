a857e6b30cd85323079e11113b477264d58edd19
pkoppineni <pkoppineni@csod.com>
2022-04-25 21:02:23 +0530

    Added contiguous array problem

--- src/LeetCodeProblems/ArrayProblems/Leetcode_525_ContiguousArray/Leetcode_525_ContiguousArray_V1.cs
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_525_ContiguousArray/Leetcode_525_ContiguousArray_V1.cs
@@ -11,7 +11,42 @@ namespace LeetCodeProblems.ArrayProblems.Leetcode_525_ContiguousArray
     {
         public int CalculateMaximumLength(int[] nums)
         {
+            if (nums.Length <= 1)
+            {
+                return 0;
+            }
+            if (nums.Length == 2)
+            {
+                return nums[0] + nums[1] == 1 ? 2 : 0;
+            }
+
             var maxLength = 0;
+            var lookUpSum = new Dictionary<int, int>();
+            var previous = 0;
+            for (var index = 0; index < nums.Length; index++)
+            {
+                if (nums[index] == 0)
+                {
+                    previous += -1;
+                }
+                else
+                {
+                    previous += 1;
+                }
+                if (previous == 0)
+                {
+                    maxLength = index + 1;
+                    continue;
+                }
+                if (!lookUpSum.ContainsKey(previous))
+                {
+                    lookUpSum.Add(previous, index);
+                }
+                else
+                {
+                    maxLength = Math.Max(maxLength, index - lookUpSum[previous]);
+                }
+            }
             return maxLength;
         }
     }
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_525_ContiguousArray/Leetcode_525_ContiguousArray_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_525_ContiguousArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_525_ContiguousArray
+{
+    [TestClass]
+    public class Leetcode_525_ContiguousArray_V1Tests
+    {
+        [TestMethod]
+        public void Given_BinaryArray_When_CalculateMaxSubArray_Then_ShouldReturnSubArrayLength()
+        {
+            //Given
+            var binaryArray = new int[] { 0, 1, 1 };
+            var problem = new Leetcode_525_ContiguousArray_V1();
+            var expectedLength = 2;
+
+            //When
+            var actualLength = problem.CalculateMaximumLength(binaryArray);
+
+            //Then
+            Assert.AreEqual(expectedLength, actualLength);
+        }
+    }
+}
