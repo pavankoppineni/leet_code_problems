57cfe562d7df82b3cabe105d5fee0edcc94d57a9
pkoppineni <pkoppineni@csod.com>
2022-05-10 19:08:58 +0530

    Added new problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_1253_ReconstructTwoRowBinaryMatrix/Leetcode_1253_ReconstructTwoRowBinaryMatrix_V1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Greedy.Leetcode_1253_ReconstructTwoRowBinaryMatrix
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reconstruct-a-2-row-binary-matrix/
+    /// </summary>
+    public class Leetcode_1253_ReconstructTwoRowBinaryMatrix_V1
+    {
+        public IList<IList<int>> ConstructBinaryMatrix(int upperSum, int lowerSum, int[] columnSum)
+        {
+            var binaryMatrix = new List<IList<int>>(2)
+            {
+                new List<int>(columnSum.Length),
+                new List<int>(columnSum.Length)
+            };
+            for (var colIndex = 0; colIndex < columnSum.Length; colIndex++)
+            {
+                binaryMatrix[0].Add(0);
+                binaryMatrix[1].Add(0);
+                if (columnSum[colIndex] == 2)
+                {
+                    columnSum[colIndex] = 0;
+                    upperSum -= 1;
+                    lowerSum -= 1;
+                    binaryMatrix[0][colIndex] = 1;
+                    binaryMatrix[1][colIndex] = 1;
+                }
+            }
+            for (var colIndex = 0; colIndex < columnSum.Length; colIndex++)
+            {
+                if (columnSum[colIndex] == 1)
+                {
+                    if (upperSum > 0)
+                    {
+                        upperSum -= 1;
+                        columnSum[colIndex] -= 1;
+                        binaryMatrix[0][colIndex] = 1;
+                    }
+                    else
+                    {
+                        lowerSum -= 1;
+                        columnSum[colIndex] -= 1;
+                        binaryMatrix[1][colIndex] = 1;
+                    }
+                }
+            }
+            if (upperSum == 0 && lowerSum == 0)
+            {
+                return binaryMatrix;
+            }
+            return new List<IList<int>>();
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_1253_ReconstructTwoRowBinaryMatrix/Leetcode_1253_ReconstructTwoRowBinaryMatrix_V1Tests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_1253_ReconstructTwoRowBinaryMatrix;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_1253_ReconstructTwoRowBinaryMatrix
+{
+    [TestClass]
+    public class Leetcode_1253_ReconstructTwoRowBinaryMatrix_V1Tests
+    {
+        [TestMethod]
+        public void Given_RowSumAndColumnSum_When_ConstructBinaryMatrix_Then_ShouldReturnBinaryMatrix()
+        {
+            //Given
+            var upperSum = 5;
+            var lowerSum = 5;
+            var columnSum = new int[] { 2, 1, 2, 0, 1, 0, 1, 2, 0, 1 };
+            var problem = new Leetcode_1253_ReconstructTwoRowBinaryMatrix_V1();
+            var expectedMatrixLength = 2;
+
+            //When
+            var actualMatrix = problem.ConstructBinaryMatrix(upperSum, lowerSum, columnSum);
+
+            //Then
+            Assert.AreEqual(expectedMatrixLength, actualMatrix.Count);
+        }
+    }
+}
