0bbdea0bb7e128a08e96010ff19662ab837cb116
pkoppineni <pkoppineni@csod.com>
2022-05-10 15:24:50 +0530

    Added new problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_1605_FindValidMatrixGivenRowAndColumnSums/Leetcode_1605_FindValidMatrixGivenRowAndColumnSums_V1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Greedy.Leetcode_1605_FindValidMatrixGivenRowAndColumnSums
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
+    /// </summary>
+    public class Leetcode_1605_FindValidMatrixGivenRowAndColumnSums_V1
+    {
+        public int[][] ConstructMatrix(int[] rowSum, int[] columnSum)
+        {
+            var matrix = new int[rowSum.Length][];
+            for (var rowIndex = 0; rowIndex < rowSum.Length; rowIndex++)
+            {
+                matrix[rowIndex] = new int[columnSum.Length];
+                for (var columnIndex = 0; columnIndex < columnSum.Length; columnIndex++)
+                {
+                    var row = rowSum[rowIndex];
+                    var column = columnSum[columnIndex];
+                    matrix[rowIndex][columnIndex] = Math.Min(row, column);
+                    rowSum[rowIndex] -= matrix[rowIndex][columnIndex];
+                    columnSum[columnIndex] -= matrix[rowIndex][columnIndex];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_1605_FindValidMatrixGivenRowAndColumnSums/Leetcode_1605_FindValidMatrixGivenRowAndColumnSums_V1Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_1605_FindValidMatrixGivenRowAndColumnSums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_1605_FindValidMatrixGivenRowAndColumnSums
+{
+    [TestClass]
+    public class Leetcode_1605_FindValidMatrixGivenRowAndColumnSums_V1Tests
+    {
+        [TestMethod]
+        public void Given_RowSumAndColumnSum_When_ConstructMatrix_Then_ShouldReturnMatrixWithRowSumAndColumnSum()
+        {
+            //Given
+            var rowSum = new int[] { 3, 8 };
+            var columnSum = new int[] { 4, 7 };
+            var problem = new Leetcode_1605_FindValidMatrixGivenRowAndColumnSums_V1();
+            var expectedMatrix = new int[][]
+            {
+                new int[]{3,0},
+                new int[]{1,7}
+            };
+
+            //When
+            var actualMatrix = problem.ConstructMatrix(rowSum, columnSum);
+
+            //Then
+            Assert.AreEqual(expectedMatrix.Length, actualMatrix.Length);
+        }
+    }
+}
