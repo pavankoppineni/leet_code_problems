57da2a7e1dbc0e51ab3196a1b10366502d68e3b7
pkoppineni <pkoppineni@csod.com>
2022-05-20 11:42:29 +0530

    Added new problem

---
+++ src/LeetCodeProblems/MatrixProblems/Leetcode_1222_QueensThatCanAttackKing/Leetcode_1222_QueensThatCanAttackKing_V1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems.Leetcode_1222_QueensThatCanAttackKing
+{
+    /// <summary>
+    /// https://leetcode.com/problems/queens-that-can-attack-the-king/
+    /// </summary>
+    public class Leetcode_1222_QueensThatCanAttackKing_V1
+    {
+        private IDictionary<int, int[]> _queensLookup;
+        private IList<IList<int>> _queens;
+
+        public IList<IList<int>> Calculate(int[][] queens, int[] king)
+        {
+            _queens = new List<IList<int>>();
+            _queensLookup = new Dictionary<int, int[]>();
+            foreach (var queen in queens)
+            {
+                _queensLookup.Add(queen[0] * 8 + queen[1], queen);
+            }
+            CalculateDiagonalAttacks(king);
+            CalculateHorizontalAttacks(king);
+            CalculateVerticalAttacks(king);
+            return _queens;
+        }
+
+        private void CalculateVerticalAttacks(int[] king)
+        {
+            var row = king[0];
+            var col = king[1];
+
+            //Action : Calculate top queens
+            for (var currentRow = row - 1; currentRow >= 0; currentRow--)
+            {
+                var pos = currentRow * 8 + col;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { currentRow, col });
+                    break;
+                }
+            }
+
+            //Action : Calculate bottom queens
+            for (var currentRow = row + 1; currentRow < 8; currentRow++)
+            {
+                var pos = currentRow * 8 + col;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { currentRow, col });
+                    break;
+                }
+            }
+        }
+
+        private void CalculateHorizontalAttacks(int[] king)
+        {
+            var row = king[0];
+            var col = king[1];
+
+            //Action : Calculate right queens
+            for (var column = col + 1; column < 8; column++)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+            }
+
+            //Action : Calculate left queens
+            for (var column = col - 1; column >= 0; column--)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+            }
+        }
+
+        private void CalculateDiagonalAttacks(int[] king)
+        {
+            var row = king[0] + 1;
+            var column = king[1] + 1;
+            while (row < 8 && column < 8)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+                row++;
+                column++;
+            }
+
+            row = king[0] - 1;
+            column = king[1] - 1;
+            while (row >= 0 && column >= 0)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+                row--;
+                column--;
+            }
+
+            row = king[0] - 1;
+            column = king[1] + 1;
+            while (row >= 0 && column < 8)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+                row--;
+                column++;
+            }
+
+            row = king[0] + 1;
+            column = king[1] - 1;
+            while (row < 8 && column >= 0)
+            {
+                var pos = row * 8 + column;
+                if (_queensLookup.ContainsKey(pos))
+                {
+                    _queens.Add(new int[] { row, column });
+                    break;
+                }
+                row++;
+                column--;
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_1222_QueensThatCanAttackKing/Leetcode_1222_QueensThatCanAttackKing_V1Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.MatrixProblems.Leetcode_1222_QueensThatCanAttackKing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems.Leetcode_1222_QueensThatCanAttackKing
+{
+    [TestClass]
+    public class Leetcode_1222_QueensThatCanAttackKing_V1Tests
+    {
+        [TestMethod]
+        public void Given_Matrix_When_CalculateQueensThatCanAttacKing_Then_ShouldReturnNumberOfQueensThatCanAttack()
+        {
+            //Given
+            var queens = new int[6][]
+            {
+                new int[]{0,1},
+                new int[]{1,0},
+                new int[]{4,0},
+                new int[]{0,4},
+                new int[]{3,3},
+                new int[]{2,4}
+            };
+            var king = new int[] { 0, 0 };
+            var problem = new Leetcode_1222_QueensThatCanAttackKing_V1();
+            var expectedCount = 3;
+
+            //When
+            var queents = problem.Calculate(queens, king);
+
+            //Then
+            Assert.AreEqual(expectedCount, queents.Count);
+        }
+    }
+}
