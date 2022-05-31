aed6b19215669df4e3024e57c07a506bb53ffbeb
pkoppineni <pkoppineni@csod.com>
2022-05-31 16:18:48 +0530

    Added new problem

---
+++ src/LeetCodeProblems/Greedy/Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers/Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers_V1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeProblems.Greedy.Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers
+{
+    /// <summary>
+    /// https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+    /// </summary>
+    public class Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers_V1
+    {
+        public bool IsPossibleDivide(int[] nums, int k)
+        {
+            var lookup = new SortedDictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (lookup.ContainsKey(num))
+                {
+                    lookup[num]++;
+                }
+                else
+                {
+                    lookup.Add(num, 1);
+                }
+            }
+            bool result;
+            do
+            {
+                result = RemoveSubset(lookup, k);
+            } while (result && lookup.Count > 0);
+            return result;
+        }
+
+        private bool RemoveSubset(IDictionary<int, int> lookup, int k)
+        {
+            var min = lookup.Keys.First();
+            while (k > 0)
+            {
+                if (lookup.ContainsKey(min))
+                {
+                    lookup[min]--;
+                    if (lookup[min] == 0)
+                    {
+                        lookup.Remove(min);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                min += 1;
+                k--;
+            }
+            return k == 0;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/Greedy/Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers/Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.Greedy.Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.Greedy.Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers
+{
+    [TestClass]
+    public class Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_DivideArray_Then_ShouldReturnTrueIfArrayCanBeDivided()
+        {
+            //Given
+            var values = new int[] { 3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11 };
+            var problem = new Leetcode_1296_DivideArrayInSetsOfKConsecutiveNumbers_V1();
+            var expectedResult = true;
+            var subArraySize = 3;
+
+            //When
+            var actualResult = problem.IsPossibleDivide(values, subArraySize);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
