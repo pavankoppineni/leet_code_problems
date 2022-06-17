4980ce2ed86b68628ab12fd7c2a766a5d624b8ab
pkoppineni <pkoppineni@csod.com>
2022-06-17 21:29:37 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_2079_WateringPlants/Leetcode_2079_WateringPlants_V1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_2079_WateringPlants
+{
+    /// <summary>
+    /// https://leetcode.com/problems/watering-plants/
+    /// </summary>
+    public class Leetcode_2079_WateringPlants_V1
+    {
+        public int CalculateSteps(int[] plants, int capacity)
+        {
+            var actualCapacity = capacity;
+            var steps = 0;
+            for (var index = 0; index < plants.Length; index++)
+            {
+                var plant = plants[index];
+
+                //Case 1 : When plant is greater than capacity
+                if (plant > capacity)
+                {
+                    capacity = actualCapacity;
+                    steps += index;
+                    steps += index + 1;
+                    capacity -= plant;
+                }
+                //Case 2 : When plant is less than capacity
+                else
+                {
+                    steps++;
+                    capacity -= plant;
+                }
+
+            }
+            return steps;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_2079_WateringPlants/Leetcode_2079_WateringPlants_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_2079_WateringPlants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_2079_WateringPlants
+{
+    [TestClass]
+    public class Leetcode_2079_WateringPlants_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfPlantsAndCapacity_When_CalculateSteps_Then_ShouldCalculateSteps()
+        {
+            //Given
+            var plants = new int[] { 3,2,4,2,1 };
+            var capacity = 6;
+            var problem = new Leetcode_2079_WateringPlants_V1();
+            var expectedResult = 17;
+
+            //When
+            var actualResult = problem.CalculateSteps(plants, capacity);
+
+            //Then
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+    }
+}
