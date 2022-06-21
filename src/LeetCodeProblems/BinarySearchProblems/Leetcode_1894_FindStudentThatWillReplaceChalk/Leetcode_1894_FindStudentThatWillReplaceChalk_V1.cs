5f240e40ae2d6d332e065b8b58d6f956771c3977
pkoppineni <pkoppineni@csod.com>
2022-06-21 15:00:49 +0530

    Added new problem

---
+++ src/LeetCodeProblems/BinarySearchProblems/Leetcode_1894_FindStudentThatWillReplaceChalk/Leetcode_1894_FindStudentThatWillReplaceChalk_V1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.BinarySearchProblems.Leetcode_1894_FindStudentThatWillReplaceChalk
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Leetcode_1894_FindStudentThatWillReplaceChalk_V1
+    {
+        private int[] _students;
+        public int FindStudent(int[] students, int k)
+        {
+            _students = students;
+            for (var index = 1; index < students.Length; index++)
+            {
+                students[index] += students[index - 1];
+            }
+            var factor = k / students[students.Length - 1];
+            var remaining = k - (factor * students[students.Length - 1]);
+            if (remaining < students[0])
+            {
+                return 0;
+            }
+
+            return FindStudent(0, students.Length - 1, remaining);
+        }
+
+        private int FindStudent(int start, int end, int value)
+        {
+            if (start == end)
+            {
+                return start;
+            }
+            var mid = start + (end - start) / 2;
+
+            //Case 1 : When mid is equal to value
+            if (_students[mid] == value)
+            {
+                return mid + 1;
+            }
+
+            //Case 2 : When mid is greater than value
+            if (value > _students[mid])
+            {
+                return FindStudent(mid + 1, end, value);
+            }
+            return FindStudent(start, mid, value);
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/BinarySearchProblems/Leetcode_1894_FindStudentThatWillReplaceChalk/Leetcode_1894_FindStudentThatWillReplaceChalk_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.BinarySearchProblems.Leetcode_1894_FindStudentThatWillReplaceChalk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.BinarySearchProblems.Leetcode_1894_FindStudentThatWillReplaceChalk
+{
+    [TestClass]
+    public class Leetcode_1894_FindStudentThatWillReplaceChalk_V1Tests
+    {
+        [TestMethod]
+        public void Given_StudentsAndCapacity_When_FindStudent_Then_ShouldReturnStudentIndex()
+        {
+            //Given
+            var students = new int[] { 3, 4, 1, 3 };
+            var capacity = 22;
+            var problem = new Leetcode_1894_FindStudentThatWillReplaceChalk_V1();
+            var expectedResult = 0;
+
+            //When
+            var actualResult = problem.FindStudent(students, capacity);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
