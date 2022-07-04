2554ad95c495d6f103ed4129cd6fb3982d9ff3e2
pkoppineni <pkoppineni@csod.com>
2022-07-04 16:23:09 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_2256_MinimumAverageDifference/Leetcode_2256_MinimumAverageDifference_V1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_2256_MinimumAverageDifference
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-average-difference/
+    /// </summary>
+    public class Leetcode_2256_MinimumAverageDifference_V1
+    {
+        public int CalculateMinimumAverageDifference(int[] values)
+        {
+            var minIndex = -1;
+            double minimumAvgDifference = int.MaxValue;
+            long totalSum = 0;
+            for (var index = 0; index < values.Length; index++)
+            {
+                totalSum += values[index];
+            }
+
+            long leftSum = 0;
+            for (var index = 0; index < values.Length - 1; index++)
+            {
+                leftSum += values[index];
+                double leftAverage = leftSum / (index + 1);
+                var rightSum = totalSum - leftSum;
+                double rightAverage = rightSum / (values.Length - (index + 1));
+                var difference = Math.Abs(leftAverage - rightAverage);
+                if (difference < minimumAvgDifference)
+                {
+                    minimumAvgDifference = difference;
+                    minIndex = index;
+                }
+            }
+
+            double average = totalSum / values.Length;
+            if (average < minimumAvgDifference)
+            {
+                minIndex = values.Length - 1;
+            }
+
+            return minIndex;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_2256_MinimumAverageDifference/Leetcode_2256_MinimumAverageDifference_V1Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_2256_MinimumAverageDifference;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_2256_MinimumAverageDifference
+{
+    [TestClass]
+    public class Leetcode_2256_MinimumAverageDifference_V1Tests
+    {
+        [TestMethod]
+        public void Given_Values_When_CalculateMinimumAverageDifference_Then_ShouldMinimumAverageDifferenceIndex()
+        {
+            //Given
+            var values = new int[] { 2, 5, 3, 9, 5, 3 };
+            var problem = new Leetcode_2256_MinimumAverageDifference_V1();
+            var expectedResult = 3;
+
+            //When
+            var actualResult = problem.CalculateMinimumAverageDifference(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
