5dea5e62aa5576267ad7ec5fdd14caa825340226
pkoppineni <pkoppineni@csod.com>
2022-07-10 20:04:53 +0530

    Added new problem

---
+++ src/LeetCodeProblems/ArrayProblems/Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors/Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors_V1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.ArrayProblems.Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
+    /// </summary>
+    public class Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors_V1
+    {
+        public int CalculateMaximumConsecutiveFloors(int bottom, int top, int[] specialFloors)
+        {
+            Array.Sort(specialFloors);
+            var max = 0;
+            for (var index = 1; index < specialFloors.Length; index++)
+            {
+                var currentMax = specialFloors[index] - specialFloors[index - 1];
+                if (currentMax > max)
+                {
+                    max = currentMax;
+                }
+            }
+            max = Math.Max(specialFloors[0] - bottom, max);
+            max = Math.Max(top - specialFloors[specialFloors.Length - 1], max);
+            return max;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/ArrayProblems/Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors/Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors_V1Tests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.ArrayProblems.Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.ArrayProblems.Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors
+{
+    [TestClass]
+    public class Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors_V1Tests
+    {
+        [TestMethod]
+        public void Given_FloorsAndSpecialFloors_When_CalculateConsecutiveFloors_Then_ShouldReturnConsecutiveFloors()
+        {
+            //Given
+            var top = 9;
+            var bottom = 2;
+            var specialFloors = new int[] { 4, 6 };
+            var problem = new Leetcode_2274_MaximumConsecutiveFloorsWithoutSpecialFloors_V1();
+            var expectedResult = 3;
+            
+            //When
+            var actualResult = problem.CalculateMaximumConsecutiveFloors(bottom, top, specialFloors);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
