f1172f3fb950798b7480b75b26918cc55e2e4a4d
pkoppineni <pkoppineni@csod.com>
2022-07-24 19:46:50 +0530

    Added new sliding window problem

---
+++ src/LeetCodeProblems/SlidingWindowProblems/Leetcode_2348_NumberOfZeroFilledSubarrays/Leetcode_2348_NumberOfZeroFilledSubarrays_V1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.SlidingWindowProblems.Leetcode_2348_NumberOfZeroFilledSubarrays
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-zero-filled-subarrays/
+    /// </summary>
+    public class Leetcode_2348_NumberOfZeroFilledSubarrays_V1
+    {
+        public long CalculateNumberOfSubArrays(int[] values)
+        {
+            var start = -1;
+            long count = 0;
+            for (var index = 0; index < values.Length; index++)
+            {
+                var value = values[index];
+                //Case 1 : When value is 0
+                if (value == 0)
+                {
+                    if (start == -1)
+                    {
+                        start = index;
+                    }
+                    var len = index - start + 1;
+                    count += len;
+                    continue;
+                }
+                start = -1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/SlidingWindowProblems/Leetcode_2348_NumberOfZeroFilledSubarrays/Leetcode_2348_NumberOfZeroFilledSubarrays_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.SlidingWindowProblems.Leetcode_2348_NumberOfZeroFilledSubarrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.SlidingWindowProblems.Leetcode_2348_NumberOfZeroFilledSubarrays
+{
+    [TestClass]
+    public class Leetcode_2348_NumberOfZeroFilledSubarrays_V1Tests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] {1,3,0,0,2,0,0,4 }, 6)]
+        [DataRow(new int[] { 0, 0, 0, 2, 0, 0 }, 9)]
+        [DataRow(new int[] { 1, 2, 3 }, 0)]
+        public void Given_ArrayOfIntegers_When_CalculateNumberOfSubarrays_Then_ShouldReturnCountOfSubarrays(int[] values, int expectedCount)
+        {
+            //Given
+            var problem = new Leetcode_2348_NumberOfZeroFilledSubarrays_V1();
+
+            //When
+            var actualCount = problem.CalculateNumberOfSubArrays(values);
+
+            //Then
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
