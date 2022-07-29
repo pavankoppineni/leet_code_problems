08b124d4d4b720c608910974e01f0123d4a11c74
pkoppineni <pkoppineni@csod.com>
2022-07-29 21:06:50 +0530

    Added new probelm

---
+++ src/LeetCodeProblems/MatrixProblems/Leetcode_2352_EqualRowAndColumnPairs/Leetcode_2352_EqualRowAndColumnPairs_V1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.MatrixProblems.Leetcode_2352_EqualRowAndColumnPairs
+{
+    /// <summary>
+    /// https://leetcode.com/problems/equal-row-and-column-pairs/
+    /// </summary>
+    public class Leetcode_2352_EqualRowAndColumnPairs_V1
+    {
+        public int CalculateEqualPairs(int[][] grid)
+        {
+            var rows = grid.Length;
+            var cols = grid[0].Length;
+            var sumLookup = new Dictionary<string, Pair>();
+            for (var row = 0; row < rows; row++)
+            {
+                var currentRowHash = string.Empty;
+                for (var col = 0; col < cols; col++)
+                {
+                    currentRowHash = $"{currentRowHash}_{grid[row][col]}";
+                }
+                if (!sumLookup.ContainsKey(currentRowHash))
+                {
+                    sumLookup.Add(currentRowHash, new Pair());
+                }
+                sumLookup[currentRowHash].RowCount += 1;
+            }
+
+            for (var col = 0; col < cols; col++)
+            {
+                var currentColumnHash = string.Empty;
+                for (var row = 0; row < rows; row++)
+                {
+                    currentColumnHash = $"{currentColumnHash}_{grid[row][col]}";
+                }
+                if (!sumLookup.ContainsKey(currentColumnHash))
+                {
+                    sumLookup.Add(currentColumnHash, new Pair());
+                }
+                sumLookup[currentColumnHash].ColCount += 1;
+            }
+
+            var numberOfPairs = 0;
+            foreach (var pair in sumLookup)
+            {
+                numberOfPairs += pair.Value.CalculatePairs();
+            }
+            return numberOfPairs;
+        }
+
+        public class Pair
+        {
+            public int ColCount { get; set; }
+            public int RowCount { get; set; }
+
+            public int CalculatePairs()
+            {
+                return ColCount * RowCount;
+            }
+        }
+    }
+}
---
+++ test/LeetCodeProblems.Tests/MatrixProblems/Leetcode_2352_EqualRowAndColumnPairs/Leetcode_2352_EqualRowAndColumnPairs_V1Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeProblems.MatrixProblems.Leetcode_2352_EqualRowAndColumnPairs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Tests.MatrixProblems.Leetcode_2352_EqualRowAndColumnPairs
+{
+    [TestClass]
+    public class Leetcode_2352_EqualRowAndColumnPairs_V1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerGrid_When_CalculateNumberOfPairs_Then_ShouldReturnNumberOfPairs()
+        {
+            //Given
+            var grid = new int[3][];
+            grid[0] = new int[] { 3, 2, 1 };
+            grid[1] = new int[] { 1, 7, 6 };
+            grid[2] = new int[] { 2, 7, 7 };
+            var problem = new Leetcode_2352_EqualRowAndColumnPairs_V1();
+            var expectedNumberOfPairs = 1;
+
+            //When
+            var actualNumberOfPairs = problem.CalculateEqualPairs(grid);
+
+            //Then
+            Assert.AreEqual(expectedNumberOfPairs, actualNumberOfPairs);
+        }
+    }
+}
